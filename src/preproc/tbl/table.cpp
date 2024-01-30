1g4-mirror/groff

commit 4f4b79b8aa0f044a3b2e6a97c8e7dd745b5eead9

[tbl]: Handle `\R` sequences in text blocks robustly.
* src/preproc/tbl/table.cpp (table::add_entry): Fix SEGV when repeating
  glyph table entry syntax (`\Rx`) used in a text block.  Lift
  extraction of entry string to be done unconditionally, rather than in
  5 different special cases.  This frees us up to rewrite the entry if
  necessary, changing '\R' to '\&' inside a text block.  Recast
  diagnostic to describe the problem clearly--"bad repeated character"
  suggests that something is wrong with the "argument" to `\R`, when
  really the problem is the _context_.

Fixes <http://savannah.gnu.org/bugs/?62366>.

src/preproc/tbl/table.cpp
@@ -1508,6 +1508,18 @@ void table::add_entry(int r, int c, const string &str,
 {
   allocate(r);
   table_entry *e = 0;
+  char *s = str.extract();
+  if (str.search('\n') >= 0) {
+    bool was_changed = false;
+    for (int i = 0; s[i] != '\0'; i++)
+      if ((i > 0) && (s[(i - 1)] == '\\') && (s[i] == 'R')) {
+	s[i] = '&';
+	was_changed = true;
+      }
+    if (was_changed)
+      error_with_file_and_line(fn, ln, "repeating a glyph with '\\R'"
+			       " is not allowed in a text block");
+  }
   if (str == "\\_") {
     e = new short_line_entry(this, f);
   }
@@ -1546,17 +1558,8 @@ void table::add_entry(int r, int c, const string &str,
     else
       do_vspan(r, c);
   }
-  else if (str.length() > 2 && str[0] == '\\' && str[1] == 'R') {
-    if (str.search('\n') >= 0)
-      error_with_file_and_line(fn, ln, "bad repeated character");
-    else {
-      char *s = str.substring(2, str.length() - 2).extract();
-      e = new repeated_char_entry(this, f, s);
-    }
-  }
   else {
     int is_block = str.search('\n') >= 0;
-    char *s;
     switch (f->type) {
     case FORMAT_SPAN:
       assert(str.empty());
@@ -1564,7 +1567,6 @@ void table::add_entry(int r, int c, const string &str,
       break;
     case FORMAT_LEFT:
       if (!str.empty()) {
-	s = str.extract();
 	if (is_block)
 	  e = new left_block_entry(this, f, s);
 	else
@@ -1575,7 +1577,6 @@ void table::add_entry(int r, int c, const string &str,
       break;
     case FORMAT_CENTER:
       if (!str.empty()) {
-	s = str.extract();
 	if (is_block)
 	  e = new center_block_entry(this, f, s);
 	else
@@ -1586,7 +1587,6 @@ void table::add_entry(int r, int c, const string &str,
       break;
     case FORMAT_RIGHT:
       if (!str.empty()) {
-	s = str.extract();
 	if (is_block)
 	  e = new right_block_entry(this, f, s);
 	else
@@ -1597,7 +1597,6 @@ void table::add_entry(int r, int c, const string &str,
       break;
     case FORMAT_NUMERIC:
       if (!str.empty()) {
-	s = str.extract();
 	if (is_block) {
 	  error_with_file_and_line(fn, ln, "can't have numeric text block");
 	  e = new left_block_entry(this, f, s);
@@ -1615,7 +1614,6 @@ void table::add_entry(int r, int c, const string &str,
       break;
     case FORMAT_ALPHABETIC:
       if (!str.empty()) {
-	s = str.extract();
 	if (is_block)
 	  e = new alphabetic_block_entry(this, f, s);
 	else

commit 6a99160c8359aff04880295c6d37586e80298f5a

[tbl]: Fix Savannah #65225.
* src/preproc/tbl/table.cpp (table::add_entry): Fix regression in
  repeated glyph tbl(1) feature (`\R`), rendering it inoperative.
  Problem introduced by me in commit 4f4b79b8aa, 26 April 2022.  Restore
  logic to handle this table entry type.  Also throw new error
  diagnostic if the repeated glyph token appears with no argument.  Also
  improve code style by reusing variable with already-computed entry
  string length instead of calculating it again.

Fixes <https://savannah.gnu.org/bugs/?65225>.  Thanks to the anonymous
submitter for a reproducing case and a correct suggestion of the
offending commit.

src/preproc/tbl/table.cpp
@@ -1581,6 +1581,16 @@ void table::add_entry(int r, int c, const string &str,
     else
       do_vspan(r, c);
   }
+  else if (strncmp(s, "\\R", 2) == 0) {
+    if (len < 3) {
+      error("'\\R' requires a glyph to repeat");
+      e = new empty_entry(this, f);
+    }
+    else {
+      char *glyph = str.substring(2, len - 2).extract();
+      e = new repeated_char_entry(this, f, glyph);
+    }
+  }
   else {
     int is_block = str.search('\n') >= 0;
     switch (f->type) {