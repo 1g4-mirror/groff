826c1ce41b1058877044c37e75cf81bf43ab4008
G. Branden Robinson <g.branden.robinson@gmail.com>
2024-03-26 21:30:15 -0500

    src/utils/grog/grog.pl: Recognize new requests.
    * src/utils/grog/grog.pl (do_line): Recognize new requests in
      forthcoming groff 1.24 release.

--- src/utils/grog/grog.pl
+++ src/utils/grog/grog.pl
@@ -282,7 +282,7 @@ sub do_line {
   # If the line calls a user-defined macro, skip it.
   return if (exists $user_macro{$command});
 
-  # These are all requests supported by groff 1.23.0.
+  # These are all requests supported by groff 1.24.0.
   my @request = ('ab', 'ad', 'af', 'aln', 'als', 'am', 'am1', 'ami',
 		 'ami1', 'as', 'as1', 'asciify', 'backtrace', 'bd',
 		 'blm', 'box', 'boxa', 'bp', 'br', 'brp', 'break', 'c2',
@@ -294,13 +294,14 @@ sub do_line {
 		 'evc', 'ex', 'fam', 'fc', 'fchar', 'fcolor', 'fi',
 		 'fp', 'fschar', 'fspecial', 'ft', 'ftr', 'fzoom',
 		 'gcolor', 'hc', 'hcode', 'hla', 'hlm', 'hpf', 'hpfa',
-		 'hpfcode', 'hw', 'hy', 'hym', 'hys', 'ie', 'if', 'ig',
-		 'in', 'it', 'itc', 'kern', 'lc', 'length', 'linetabs',
-		 'lf', 'lg', 'll', 'lsm', 'ls', 'lt', 'mc', 'mk', 'mso',
-		 'msoquiet', 'na', 'ne', 'nf', 'nh', 'nm', 'nn', 'nop',
-		 'nr', 'nroff', 'ns', 'nx', 'open', 'opena', 'os',
-		 'output', 'pc', 'pev', 'pi', 'pl', 'pm', 'pn', 'pnr',
-		 'po', 'ps', 'psbb', 'pso', 'ptr', 'pvs', 'rchar', 'rd',
+		 'hpfcode', 'hw', 'hy', 'hydefault', 'hym', 'hys', 'ie',
+		 'if', 'ig', 'in', 'it', 'itc', 'kern', 'lc', 'length',
+		 'linetabs', 'lf', 'lg', 'll', 'lsm', 'ls', 'lt', 'mc',
+		 'mk', 'mso', 'msoquiet', 'na', 'ne', 'nf', 'nh', 'nm',
+		 'nn', 'nop', 'nr', 'nroff', 'ns', 'nx', 'open',
+		 'opena', 'os', 'output', 'pc', 'pcolor', 'pcomposite',
+		 'pev', 'phw', 'pi', 'pl', 'pm', 'pn', 'pnr', 'po',
+		 'ps', 'psbb', 'pso', 'ptr', 'pvs', 'rchar', 'rd',
 		 'return', 'rfschar', 'rj', 'rm', 'rn', 'rnn', 'rr',
 		 'rs', 'rt', 'schar', 'shc', 'shift', 'sizes', 'so',
 		 'soquiet', 'sp', 'special', 'spreadwarn', 'ss',
