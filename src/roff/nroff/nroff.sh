315ba634a3d5ab14b3d882593cafed0c8bba47ea
G. Branden Robinson <g.branden.robinson@gmail.com>
2024-07-11 07:25:35 -0500

    [nroff]: Report only "basename" of $0 in diags.
    * src/roff/nroff/nroff.sh: Define `prog` variable as only the "basename"
      of argv[0], making diagnostic and usage messages less garrulous.  See
      Savannah #65110.

--- src/roff/nroff/nroff.sh
+++ src/roff/nroff/nroff.sh
@@ -20,7 +20,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-prog="$0"
+prog=${0##*/}
 
 T=
 Topt=
