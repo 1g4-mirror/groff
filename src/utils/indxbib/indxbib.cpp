1g4-mirror/groff

commit d7b36a45fc3f49f7db82f5edd33c2a66696115e5

[indxbib]: Mitigate Savannah #65452.
* src/utils/indxbib/indxbib.cpp: Validate `-h` option arguments more
  carefully.

  (main): Insist on an argument value of at least 2, since a hash table
  of size 1 is pointless.

  (check_integer_arg): Try to be more robust in the fact of C/C++'s
  notorious lax integer sizing practices.  We might consider gnulib's
  "xstrtol" module.  Check `errno` for `ERANGE` after calling
  `strtoll()` and add range-oriented fatal diagnostic.  Promote other
  `-h` argument validation errors to `fatal()`.  Only perform a
  comparison against INT_MAX if LONG_MAX is larger than INT_MAX in the
  first place.  Report the supported range in range diagnostics.  Use
  C++- instead of C-style type cast of result.

Mitigates, but arguably does not fix,
<https://savannah.gnu.org/bugs/?65452>.  Thanks to Alex Colomar for the
report.

I wanted to use `strtoll()`, but...
  error: ISO C++ 1998 does not support ‘long long’ [-Wlong-long]
...and in any case that just kicks the can to other architectures where
int, long, and long long are all 64 bits wide.

gnulib, take me away...

src/utils/indxbib/indxbib.cpp
@@ -147,7 +147,7 @@ int main(int argc, char **argv)
     case 'h':
       {
 	int requested_hash_table_size;
-	check_integer_arg('h', optarg, 1, &requested_hash_table_size);
+	check_integer_arg('h', optarg, 2, &requested_hash_table_size);
 	hash_table_size = requested_hash_table_size;
 	if ((hash_table_size > 2) && (hash_table_size % 2) == 0)
 		hash_table_size++;
@@ -343,16 +343,20 @@ static void check_integer_arg(char opt, const char *arg, int min, int *res)
 {
   char *ptr;
   long n = strtol(arg, &ptr, 10);
-  if (n == 0 && ptr == arg)
-    error("argument to -%1 not an integer", opt);
+  if (ERANGE == errno)
+    fatal("argument to -%1 must be between %2 and %3", arg, min,
+	  INT_MAX);
+  else if (n == 0 && ptr == arg)
+    fatal("argument to -%1 not an integer", opt);
   else if (n < min)
-    error("argument to -%1 must not be less than %2", opt, min);
+    fatal("argument to -%1 must not be less than %2", opt, min);
   else {
-    if (n > INT_MAX)
-      error("argument to -%1 greater than maximum integer", opt);
+    if ((LONG_MAX > INT_MAX) && (n > INT_MAX))
+      fatal("argument to -%1 must be between %2 and %3", arg, min,
+	    INT_MAX);
     else if (*ptr != '\0')
-      error("junk after integer argument to -%1", opt);
-    *res = int(n);
+      fatal("junk after integer argument to -%1", opt);
+    *res = static_cast<int>(n);
   }
 }
 

commit a4fc074b36b2a6054608eb2f7e83d7b5803b8b58

[indxbib]: Collapse related tests.
* src/utils/indxbib/indxbib.cpp (check_integer_arg): Collapse related
  tests.

Fixes: d7b36a45fc3f ("[indxbib]: Mitigate Savannah #65452.")
Link: <https://savannah.gnu.org/bugs/?65452>
Cc: "G. Branden Robinson" <branden@debian.org>
Cc: Dave Kemper <saint.snit@gmail.com>
Cc: "James K. Lowden" <jklowden@schemamania.org>
Cc: Colin Watson <cjwatson@debian.org>
Cc: Werner LEMBERG <wl@gnu.org>
Cc: James Clark <jjc@jclark.com>
Signed-off-by: Alejandro Colomar <alx@kernel.org>

src/utils/indxbib/indxbib.cpp
@@ -340,13 +340,9 @@ static void check_integer_arg(char opt, const char *arg, int min, int *res)
   char *ptr;
   errno = 0;
   long n = strtol(arg, &ptr, 10);
-  if (ERANGE == errno)
-    fatal("argument to -%1 must be between %2 and %3", arg, min, INT_MAX);
   if (ptr == arg)
     fatal("argument to -%1 not an integer", opt);
-  if (n < min)
-    fatal("argument to -%1 must not be less than %2", opt, min);
-  if (n > INT_MAX)
+  if (ERANGE == errno || n < min || n > INT_MAX)
     fatal("argument to -%1 must be between %2 and %3", arg, min, INT_MAX);
   if (*ptr != '\0')
     fatal("junk after integer argument to -%1", opt);