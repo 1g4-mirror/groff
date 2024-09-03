1g4-mirror/groff

commit 5f2704d64abb3d04477b1f2907eabdfec19e2925

[troff]: Fix Savannah #66151.
Update input token pointer appropriately when certain requests requiring
arguments are invalidly invoked without them.

* src/roff/troff/env.cpp (override_sizes):
* src/roff/troff/input.cpp (while_request, pipe_output, system_request):
  Check for arguments: if none are present, throw warning in category
  "missing" and skip the remainder of the input line.

  (pipe_output, system_request): Add some assertions for paranoia's
  sake.

Fixes <https://savannah.gnu.org/bugs/?66151>.

src/roff/troff/env.cpp
@@ -1325,6 +1325,12 @@ void point_size()
 
 void override_sizes()
 {
+  if (!has_arg()) {
+    warning(WARN_MISSING, "available font sizes override request"
+	    " expects at least one argument");
+    skip_line();
+    return;
+  }
   int n = 16;
   int *sizes = new int[n]; // C++03: new int[n]();
   (void) memset(sizes, 0, (n * sizeof(int)));

src/roff/troff/input.cpp
@@ -1890,7 +1890,10 @@ void token::skip()
     next();
 }
 
-// Specify `want_peek` if request reads the next argument in copy mode.
+// Specify `want_peek` if request reads the next argument in copy mode,
+// or otherwise must interpret it specially, as when reading a
+// conditional expression (`if`, `ie`, `while`), or expecting a
+// delimited argument (`tl`).
 bool has_arg(bool want_peek)
 {
   if (tok.is_newline())
@@ -6334,7 +6337,11 @@ static bool want_loop_break = false;
 
 static void while_request()
 {
-  // We can't use `has_arg()` here.  XXX: Figure out why.
+  if (!has_arg(true /* peek */)) {
+    warning(WARN_MISSING, "while loop request expects arguments");
+    skip_line();
+    return;
+  }
   macro mac;
   bool is_char_escaped = false;
   int level = 0;
@@ -8164,6 +8171,12 @@ char *read_string()
 
 void pipe_output()
 {
+  if (!has_arg()) {
+    warning(WARN_MISSING, "device-independent output piping request"
+	    " expects a system command as argument");
+    skip_line();
+    return;
+  }
   if (!want_unsafe_requests) {
     error("'pi' request is not allowed in safer mode");
     skip_line();
@@ -8175,6 +8188,8 @@ void pipe_output()
     }
     else {
       char *pc = read_string();
+      // This shouldn't happen thanks to `has_arg()` above.
+      assert(pc != 0 /* nullptr */);
       if (0 /* nullptr */ == pc)
 	error("cannot apply pipe request to empty command");
       // Are we adding to an existing pipeline?
@@ -8200,12 +8215,20 @@ static int system_status;
 
 void system_request()
 {
+  if (!has_arg()) {
+    warning(WARN_MISSING, "system command execution request expects a"
+	    " system command as argument");
+    skip_line();
+    return;
+  }
   if (!want_unsafe_requests) {
     error("'sy' request is not allowed in safer mode");
     skip_line();
   }
   else {
     char *command = read_string();
+    // This shouldn't happen thanks to `has_arg()` above.
+    assert(command != 0 /* nullptr */);
     if (0 /* nullptr */ == command)
       error("empty command");
     else {

commit f574a96f585b284ca0546064cd232e624ebefaec

[troff]: Make `pev` dump font's valid size table.
* src/roff/troff/env.cpp (override_sizes): Fix regression introduced in
  commit 5f2704d64a, yesterday.  However, it was hard to tell whether
  the request works.

  (font_size::dump_size_table): New static member function reports the
  valid size table of the environment's currently selected font.

  (environment::print_env): Report the valid size table.

* src/roff/groff/tests/sizes-request-works.sh: Test it.
* src/roff/groff/groff.am (groff_TESTS): Run test.

Fixes <https://savannah.gnu.org/bugs/?66164>.

src/roff/groff/groff.am
@@ -86,6 +86,7 @@ groff_TESTS = \
   src/roff/groff/tests/regression_savannah_58337.sh \
   src/roff/groff/tests/regression_savannah_59202.sh \
   src/roff/groff/tests/set-stroke-thickness.sh \
+  src/roff/groff/tests/sizes-request-works.sh \
   src/roff/groff/tests/soquiet-request-works.sh \
   src/roff/groff/tests/stringdown-and-stringup-requests-work.sh \
   src/roff/groff/tests/stringdown-request-rejects-request-names.sh \

src/roff/troff/env.cpp
@@ -248,6 +248,30 @@ void font_size::init_size_table(int *sizes)
   qsort(size_table, nranges, sizeof(size_range), compare_ranges);
 }
 
+void font_size::dump_size_table()
+{
+  int lo, hi;
+  errprint("  valid type size table for selected font: ");
+  if (nranges == 0)
+    errprint(" empty!");
+  else {
+    bool need_comma = false;
+    for (int i = 0; i < nranges; i++) {
+      lo = size_table[i].min;
+      hi = size_table[i].max;
+      if (need_comma)
+	errprint(", ");
+      if (lo == hi)
+	errprint("%1s", lo);
+      else
+	errprint("%1s-%2s", lo, hi);
+      need_comma = true;
+    }
+  }
+  errprint("\n");
+  fflush(stderr);
+}
+
 font_size::font_size(int sp)
 {
   for (int i = 0; i < nranges; i++) {
@@ -1323,10 +1347,9 @@ void point_size()
   skip_line();
 }
 
-// TODO: .psizes
 void override_sizes()
 {
-  if (!has_arg()) {
+  if (!has_arg(true /* peek */)) {
     warning(WARN_MISSING, "available font sizes override request"
 	    " expects at least one argument");
     skip_line();
@@ -3437,6 +3460,7 @@ void environment::print_env()
     errprint("  previous requested type size: %1s\n",
 	     prev_requested_size);
     errprint("  requested type size: %1s\n", requested_size);
+    font_size::dump_size_table();
   }
   errprint("  previous font selection: %1 ('%2')\n", prev_fontno,
 	   get_font_name(prev_fontno, this).contents());

src/roff/troff/env.h
@@ -36,6 +36,7 @@ public:
   int operator==(font_size);
   int operator!=(font_size);
   static void init_size_table(int *sizes);
+  static void dump_size_table();
 };
 
 inline font_size::font_size() : p(0)

src/roff/groff/tests/sizes-request-works.sh
@@ -1,0 +1,34 @@
+#!/bin/sh
+#
+# Copyright (C) 2024 Free Software Foundation, Inc.
+#
+# This file is part of groff.
+#
+# groff is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# groff is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+groff="${abs_top_builddir:-.}/test-groff"
+
+input='.
+.pev
+.sizes 2-20 36
+.pev
+.'
+
+echo "checking that 'sizes' request works" >&2
+output=$(printf '%s\n' "$input" | "$groff" -T ps 2>&1)
+echo "$output" | grep 'valid type size table'
+echo "$output" | grep -Fq '2s-20s, 36s'
+
+# vim:set autoindent expandtab shiftwidth=2 tabstop=2 textwidth=72: