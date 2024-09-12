1g4-mirror/groff

commit 3bb13e47528b8dbe64ad0a1b504f9565086b236a

[troff]: Fix code style nits.
* src/roff/troff/input.cpp (do_open): Fix code style nits.  Use C++
  `static_cast` operator instead of C-style type casts.  Emit error
  diagnostic if `fclose()` fails.

Also annotate null pointer with `nullptr` comment to ease any future
transition to C++11, which defines it as a keyword.

src/roff/troff/input.cpp
@@ -7214,12 +7214,13 @@ static void do_open(bool append)
 	      filename.contents(),
 	      append ? "appending" : "writing",
 	      strerror(errno));
-	fp = (FILE *)stream_dictionary.remove(stream);
+	fp = static_cast<FILE *>(stream_dictionary.remove(stream));
       }
       else
-	fp = (FILE *)stream_dictionary.lookup(stream, fp);
-      if (fp)
-	fclose(fp);
+	fp = static_cast<FILE *>(stream_dictionary.lookup(stream, fp));
+      if (fp != 0 /* nullptr */ && (fclose(fp) != 0))
+	error("cannot close file '%1': %2", filename.contents(),
+	      strerror(errno));
     }
   }
   skip_line();

commit 0ff9800a5d363b9e2b2b5a2f3778d4185f7adf55

[troff]: Fix broken `write` request.
* src/roff/troff/input.cpp (do_open): The `write` request, an "unsafe
  mode" feature has been broken for a week.  (As, possibly, have others
  that write to streams.)  Revert `static_cast` operators to C-style
  casts, which seem to be necessary (sometimes) with groff's bespoke,
  pre-STL `dictionary` class implementation.  Unfortunately, when static
  casts fail, they fail silently, happily handing back null pointers
  that you might not notice right away.  Fixes problem introduced by me
  in commit 3bb13e4752, 4 September.

src/roff/troff/input.cpp
@@ -7214,10 +7214,10 @@ static void do_open(bool append)
 	      filename.contents(),
 	      append ? "appending" : "writing",
 	      strerror(errno));
-	fp = static_cast<FILE *>(stream_dictionary.remove(stream));
+	fp = (FILE *)stream_dictionary.remove(stream);
       }
       else
-	fp = static_cast<FILE *>(stream_dictionary.lookup(stream, fp));
+	fp = (FILE *)stream_dictionary.lookup(stream, fp);
       if (fp != 0 /* nullptr */ && (fclose(fp) != 0))
 	error("cannot close file '%1': %2", filename.contents(),
 	      strerror(errno));