eaa07d3241eecb1164b48b547ff75bddae502a64
G. Branden Robinson <g.branden.robinson@gmail.com>
2024-11-27 22:29:21 -0600

    src/roff/troff/hvunits.h: Drop some boilerplate.
    * src/roff/troff/hvunits.h: Drop Autoconf "<config.h>"-inclusion
      boilerplate; troff's ".cpp" files reliably have it now.

--- src/roff/troff/hvunits.h
+++ src/roff/troff/hvunits.h
@@ -16,10 +16,6 @@ for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
 #include <stdckdint.h>
 
 class vunits {
