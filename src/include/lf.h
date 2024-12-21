c706ea2a418f12a9eac9418814121a9182a4df7b
G. Branden Robinson <g.branden.robinson@gmail.com>
2024-12-21 16:16:12 -0600

    src/include/lf.h: Include "stringclass.h" header.
    * src/include/lf.h: Include "stringclass.h" header file, since we refer
      to groff's `string` type.

--- src/include/lf.h
+++ src/include/lf.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014-2020 Free Software Foundation, Inc.
+/* Copyright (C) 2014-2024 Free Software Foundation, Inc.
      Written by Werner Lemberg (wl@gnu.org)
 
 This file is part of groff.
@@ -15,6 +15,8 @@ for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include "stringclass.h"
 
 bool interpret_lf_args(const char *p);
 void normalize_for_lf (string &fn);
