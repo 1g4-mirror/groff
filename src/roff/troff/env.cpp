1g4-mirror/groff

commit d2dceabb8398c2ba79a3303483add9ff197a48ea

[troff]: Fix code style nit.
* src/roff/troff/env.cpp (environment::choose_breakpoint): Fix code
  style nit: parenthesize complex expressions.

src/roff/troff/env.cpp
@@ -2001,22 +2001,25 @@ breakpoint *environment::choose_breakpoint()
 	  }
 	  if (best_bp_fits
 	      // Decide whether to use the hyphenated breakpoint.
-	      && (hyphen_line_max < 0
-		  // Only choose the hyphenated breakpoint if it would not
-		  // exceed the maximum number of consecutive hyphenated
-		  // lines.
-		  || hyphen_line_count + 1 <= hyphen_line_max)
-	      && !(adjust_mode == ADJUST_BOTH
+	      && ((hyphen_line_max < 0)
+		  // Only choose the hyphenated breakpoint if it would
+		  // not exceed the maximum number of consecutive
+		  // hyphenated lines.
+		  || (hyphen_line_count + 1 <= hyphen_line_max))
+	      && !((adjust_mode == ADJUST_BOTH)
 		   // Don't choose the hyphenated breakpoint if the line
 		   // can be justified by adding no more than
 		   // hyphenation_space to any word space.
 		   ? (bp->nspaces > 0
-		      && (((target_text_length - bp->width
-			    + (bp->nspaces - 1)*hresolution)/bp->nspaces)
+		      && ((((target_text_length - bp->width)
+			    + ((bp->nspaces - 1) * hresolution)
+			       / bp->nspaces))
 			  <= hyphenation_space))
 		   // Don't choose the hyphenated breakpoint if the line
-		   // is no more than hyphenation_margin short.
-		   : target_text_length - bp->width <= hyphenation_margin)) {
+		   // is no more than hyphenation_margin short of the
+		   // line length.
+		   : ((target_text_length - bp->width)
+		      <= hyphenation_margin))) {
 	    delete bp;
 	    return best_bp;
 	  }

commit a0171c9dc12daa8e70b9178bfac63aa11745110b

[troff]: Fix Savannah #66723 (`hys` broken).
* src/roff/troff/env.cpp (environment::choose_breakpoint):
  Reparenthensize (very) complex expression, restoring documented
  behavior of hyphenation space feature.  Problem introduced by me in
  commit d2dceabb83, 30 August.

Fixes <https://savannah.gnu.org/bugs/?66723>.  Thanks to Dave Kemper for
the report.

"The issue's not whether you've parenthesized your complex expressions,
Lenny...the issue is whether you've parenthesized your complex
expressions _enough_."

src/roff/troff/env.cpp
@@ -2042,11 +2042,11 @@ breakpoint *environment::choose_breakpoint()
 		   // Don't choose the hyphenated breakpoint if the line
 		   // can be justified by adding no more than
 		   // hyphenation_space to any word space.
-		   ? (bp->nspaces > 0
-		      && ((((target_text_length - bp->width)
-			    + ((bp->nspaces - 1) * hresolution)
-			       / bp->nspaces))
-			  <= hyphenation_space))
+		   ? ((bp->nspaces > 0)
+		      && (((target_text_length - bp->width)
+			    + ((bp->nspaces - 1) * hresolution))
+			    / bp->nspaces)
+			   <= hyphenation_space)
 		   // Don't choose the hyphenated breakpoint if the line
 		   // is no more than hyphenation_margin short of the
 		   // line length.