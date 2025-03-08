1g4-mirror/groff

commit f0871659336bece1831c0de3b099d67f70e81bf5

[troff]: `cf`, `trf` now accept spacey file names.
The `cf` and `trf` requests now accept leading and embedded spaces in
their "file" arguments, just like `so`.

* src/roff/troff/input.cpp (copy_file, transparent_file): Gather the
  argument with `read_string()` (which reads a potentially spaceful
  argument including a discardable leading double quote), not
  `get_long_name()` (which reads a GNU troff identifier).  Call
  `tok.next()` afterwards.

* doc/groff.texi.in (I/O):
* man/groff_diff.7.man (New requests, Altered requests): Document it.

* NEWS: Update existing items.

A more general revision to groff(7) will follow.

src/roff/troff/input.cpp
@@ -8535,7 +8535,7 @@ void system_request()
 
 void copy_file()
 {
-  if (!has_arg()) {
+  if (!has_arg(true /* peek */)) {
     warning(WARN_MISSING, "file throughput request expects a file name"
 	    " as argument");
     skip_line();
@@ -8545,13 +8545,12 @@ void copy_file()
     handle_initial_request(COPY_FILE_REQUEST);
     return;
   }
-  symbol filename = get_long_name(true /* required */);
-  while (!tok.is_newline() && !tok.is_eof())
-    tok.next();
+  char *filename = read_string();
+  tok.next();
   if (want_break)
     curenv->do_break();
-  if (!filename.is_null())
-    curdiv->copy_file(filename.contents());
+  if (filename != 0 /* nullptr */)
+    curdiv->copy_file(filename);
   tok.next();
 }
 
@@ -8579,7 +8578,7 @@ void vjustify()
 
 void transparent_file()
 {
-  if (!has_arg()) {
+  if (!has_arg(true /* peek */)) {
     warning(WARN_MISSING, "transparent file throughput request expects"
 	    " a file name as argument");
     skip_line();
@@ -8589,17 +8588,15 @@ void transparent_file()
     handle_initial_request(TRANSPARENT_FILE_REQUEST);
     return;
   }
-  symbol filename = get_long_name(true /* required */);
-  while (!tok.is_newline() && !tok.is_eof())
-    tok.next();
+  char *filename = read_string();
+  tok.next();
   if (want_break)
     curenv->do_break();
-  if (!filename.is_null()) {
+  if (filename != 0 /* nullptr */) {
     errno = 0;
-    FILE *fp = include_search_path.open_file_cautious(filename.contents());
+    FILE *fp = include_search_path.open_file_cautious(filename);
     if (0 /* nullptr */ == fp)
-      error("cannot open '%1': %2", filename.contents(),
-	    strerror(errno));
+      error("cannot open '%1': %2", filename, strerror(errno));
     else {
       int bol = 1;
       for (;;) {

doc/groff.texi.in
@@ -16328,8 +16328,8 @@ the same directories as macro files for the @option{-m} command-line
 option.
 @endDefreq
 
-@DefreqList {trf, file}
-@DefreqListEndx {cf, file}
+@DefreqList {trf, [@code{"}]@Var{file}}
+@DefreqListEndx {cf, [@code{"}]@Var{file}}
 @cindex transparent output (@code{cf}, @code{trf})
 @cindex output, transparent (@code{cf}, @code{trf})
 @cindex @code{cf} request, and copy mode

man/groff_diff.7.man
@@ -4061,7 +4061,8 @@ are read in copy mode.
 .\" ====================================================================
 .
 .TP
-.BI .cf\~ file
+.BR .cf\~ [ \[dq] ]\c
+.I file
 In a diversion,
 embed an object which,
 when interpolated,
@@ -4076,6 +4077,18 @@ the contents of
 are immediately copied to the output regardless of whether a diversion
 is being written to;
 this behavior is so anomalous that it must be considered a bug.
+.
+.
+.IP
+GNU
+.I troff \" GNU
+strips a leading neutral double quote from the argument,
+allowing it to contain embedded leading spaces.
+.
+Further,
+spaces in
+.I file
+are accepted as part of the file name.
 .
 .
 .br

commit 372155264ca71c29f5170608995a77bb0b1dad2a

[troff]: Fix Savannah #66863 (`cf`, `trf` broken).
* src/roff/troff/input.cpp (copy_file, transparent_file): Drop
  extraneous `tok.next()` calls; they are already performed just prior
  to function return.  They caused the input stream pointer to advance
  too far.

Fixes <https://savannah.gnu.org/bugs/?66863>.  Problem introduced by me
in commit f087165933, 7 December.

src/roff/troff/input.cpp
@@ -8689,7 +8689,6 @@ void copy_file()
     return;
   }
   char *filename = read_string();
-  tok.next();
   if (want_break)
     curenv->do_break();
   if (filename != 0 /* nullptr */)
@@ -8733,7 +8732,6 @@ void transparent_file()
     return;
   }
   char *filename = read_string();
-  tok.next();
   if (want_break)
     curenv->do_break();
   if (filename != 0 /* nullptr */) {