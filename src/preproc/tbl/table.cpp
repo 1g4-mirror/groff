1g4-mirror/groff

commit 25ef9f6bdad8f410749388e20caaa5c2d5e9ddee

[eqn,pic,preconv,refer,soelim,tbl]: Fix lf reqs.
Use `lf` requests _even more_ carefully.

* src/preproc/eqn/main.cpp (do_file):
* src/preproc/pic/troff.cpp (troff_output::set_location):
* src/preproc/preconv/preconv.cpp (do_file):
* src/preproc/refer/refer.cpp (do_file, output_pending_line):
* src/preproc/soelim/soelim.cpp (set_location):
* src/preproc/tbl/main.cpp (main):
* src/preproc/tbl/table.cpp (set_troff_location): Output file name with
  leading double quote in generated `lf` request only if it doesn't
  already start with one.

Avoids misleading output like:

troff: backtrace: file '"man/groff_char.7':1062
troff:"man/groff_char.7:1062: warning: special character '.j' not defined

src/preproc/tbl/table.cpp
@@ -3208,7 +3208,9 @@ void set_troff_location(const char *fn, int ln)
     string filename(fn);
     filename += '\0';
     normalize_for_lf(filename);
-    printfs(".lf %1 \"%2\n", as_string(ln), filename.contents());
+    printfs(".lf 1 %1%2\n", ('"' == filename[0]) ? "" : "\"",
+	    filename.contents());
+
     last_filename = fn;
     location_force_filename = 0;
   }

src/preproc/eqn/main.cpp
@@ -106,7 +106,8 @@ void do_file(FILE *fp, const char *filename)
   current_lineno = 1;
   current_filename = fn.contents();
   if (output_format == troff)
-    (void) printf(".lf %d \"%s\n", current_lineno, current_filename);
+    (void) printf(".lf %d %s%s\n", current_lineno,
+	('"' == current_filename[0]) ? "" : "\"", current_filename);
   while (read_line(fp, &linebuf)) {
     if (linebuf.length() >= 4
 	&& linebuf[0] == '.' && linebuf[1] == 'l' && linebuf[2] == 'f'

src/preproc/pic/troff.cpp
@@ -600,7 +600,8 @@ void troff_output::set_location(const char *s, int n)
     }
   }
   if (update_file_name)
-    printf(".lf %d \"%s\n", n, s);
+    printf(".lf %d %s%s\n", current_lineno,
+	('"' == current_filename[0]) ? "" : "\"", current_filename);
   else
     printf(".lf %d\n", n);
 }

src/preproc/preconv/preconv.cpp
@@ -1197,7 +1197,8 @@ do_file(const char *filename)
     string fn(filename);
     fn += '\0';
     normalize_for_lf(fn);
-    (void) printf(".lf 1 \"%s\n", fn.contents());
+    (void) printf(".lf 1 %s%s\n", ('"' == filename[0]) ? "" : "\"",
+		  fn.contents());
   }
   int success = 1;
   // Call converter (converters write to stdout).

src/preproc/refer/refer.cpp
@@ -475,8 +475,8 @@ static void do_file(const char *filename)
   normalize_for_lf(fn);
   current_lineno = 1;
   current_filename = fn.contents();
-  (void) fprintf(outfp, ".lf %d \"%s\n", current_lineno,
-		 current_filename);
+  (void) fprintf(outfp, ".lf %d %s%s\n", current_lineno,
+	('"' == current_filename[0]) ? "" : "\"", current_filename);
   string line;
   for (;;) {
     line.clear();
@@ -691,7 +691,8 @@ static void output_pending_line()
   if (!accumulate)
     immediately_output_references();
   if (need_syncing) {
-    fprintf(outfp, ".lf %d \"%s\n", current_lineno, current_filename);
+    fprintf(outfp, ".lf %d %s%s\n", current_lineno,
+	  ('"' == current_filename[0]) ? "" : "\"", current_filename);
     need_syncing = 0;
   }
 }

src/preproc/soelim/soelim.cpp
@@ -130,7 +130,8 @@ void set_location()
 {
   if (!want_raw_output) {
     if (!want_tex_output)
-      printf(".lf %d \"%s\n", current_lineno, current_filename);
+      printf(".lf %d %s%s\n", current_lineno,
+	('"' == current_filename[0]) ? "" : "\"", current_filename);
     else
       // XXX: Should we quote the file name?  What's TeX-conventional?
       printf("%% file %s, line %d\n", current_filename, current_lineno);

src/preproc/tbl/main.cpp
@@ -1708,7 +1708,8 @@ int main(int argc, char **argv)
       if (argv[i][0] == '-' && argv[i][1] == '\0') {
 	current_lineno = 1;
 	current_filename = "-";
-	(void) printf(".lf %d \"\%s\n", current_lineno,
+	(void) printf(".lf %d %s%s\n", current_lineno,
+		      ('"' == current_filename[0]) ? "" : "\"",
 		      current_filename);
 	process_input_file(stdin);
       }
@@ -1725,7 +1726,8 @@ int main(int argc, char **argv)
 	  normalize_for_lf(fn);
 	  current_lineno = 1;
 	  current_filename = fn.contents();
-	  (void) printf(".lf %d \"\%s\n", current_lineno,
+	  (void) printf(".lf %d %s%s\n", current_lineno,
+			('"' == current_filename[0]) ? "" : "\"",
 			current_filename);
 	  process_input_file(fp);
 	}
@@ -1734,7 +1736,9 @@ int main(int argc, char **argv)
   else {
     current_lineno = 1;
     current_filename = "-";
-    (void) printf(".lf %d \"\%s\n", current_lineno, current_filename);
+    (void) printf(".lf %d %s%s\n", current_lineno,
+		  ('"' == current_filename[0]) ? "" : "\"",
+		  current_filename);
     process_input_file(stdin);
   }
   if (ferror(stdout))

commit 4a2510cb872709ca12a3912208d619424fd5649e

[tbl]: Fix Savannah #66931 (wrong `lf` requests).
* src/preproc/tbl/table.cpp (set_troff_location): Fix logic error.  When
  writing a *roff `lf` request with a file name argument, write out the
  actual corresponding input file line number instead of "1".

Fixes <https://savannah.gnu.org/bugs/?66931>.  Thanks to Bjarni Ingi
Gislason for the report.  Problem introduced by me in commit 25ef9f6bda,
26 February.

src/preproc/tbl/table.cpp
@@ -3208,8 +3208,8 @@ void set_troff_location(const char *fn, int ln)
     string filename(fn);
     filename += '\0';
     normalize_for_lf(filename);
-    printfs(".lf 1 %1%2\n", ('"' == filename[0]) ? "" : "\"",
-	    filename.contents());
+    printfs(".lf %1 %2%3\n", as_string(ln),
+	    ('"' == filename[0]) ? "" : "\"", filename.contents());
 
     last_filename = fn;
     location_force_filename = 0;