913e0ce9361a24c4007387b2eb9f9ff540879688
G. Branden Robinson <g.branden.robinson@gmail.com>
2025-03-26 16:52:36 -0500

    src/include/json-encode.h: Use `unsigned` char.
    * src/include/json-encode.h (struct json_char): Use explicitly
      `unsigned` character type when employing it as a numeric value
      (counting the number of `char`s necessary to JSON-encode a C/C++
      character).  See CMU SEI INT07-C.

--- src/include/json-encode.h
+++ src/include/json-encode.h
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 struct json_char {
-  char len;
+  unsigned char len;
   char buf[7]; // '\uXXXX' + '\0'
 };
 
