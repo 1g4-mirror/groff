ddba6a8295426a72c4559194ceccd911b151969e
G. Branden Robinson <g.branden.robinson@gmail.com>
2025-04-10 03:59:49 -0500

    [troff]: Use scaling units in `break` warnings.
    * src/roff/troff/env.cpp (environment::possibly_break_line): Improve
      "break" warning diagnostic messages when adjustment is frustrated.  In
      troff mode, use the scaling unit configured by the `warnscale`
      request.  In nroff mode, use ens (which, since they are defined in a
      typeface-proportional manner, are not an acceptable scaling unit for
      `warnscale`).
    
    * doc/groff.texi.in (Warnings):
    * src/roff/troff/troff.1.man (Warnings): Document it.
    
    * NEWS: Update item regarding output warnings in nroff mode.

--- src/roff/troff/env.cpp
+++ src/roff/troff/env.cpp
@@ -2220,6 +2220,10 @@ static bool distribute_space(node *nd, int nspaces,
   return true;
 }
 
+// from input.cpp
+extern double warn_scale;
+extern char warn_scaling_unit;
+
 void environment::possibly_break_line(bool must_break_here,
 				      bool must_adjust)
 {
@@ -2249,16 +2253,26 @@ void environment::possibly_break_line(bool must_break_here,
     hunits space_deficit = target_text_length - bp->width;
     // An overset line always gets a warning.
     if (space_deficit < H0)
-      output_warning(WARN_BREAK, "cannot %1 line; overset by %2 units",
+      output_warning(WARN_BREAK, "cannot %1 line; overset by %2%3",
 		     (ADJUST_BOTH == adjust_mode) ? "adjust" : "break",
-		     abs(space_deficit.to_units()));
+		     in_nroff_mode
+		     ? static_cast<int>(ceil(abs(space_deficit
+						 .to_units()
+					/ hresolution)))
+		     : abs(space_deficit.to_units() / warn_scale),
+		     in_nroff_mode ? 'n' : warn_scaling_unit);
     // An underset line warns only if it requires adjustment but no
     // adjustable spaces exist on the line.
     else if ((ADJUST_BOTH == adjust_mode)
 	     && (space_deficit > H0)
 	     && (0 == bp->nspaces))
-      output_warning(WARN_BREAK, "cannot adjust line; underset by %1"
-		     " units", space_deficit.to_units());
+      output_warning(WARN_BREAK, "cannot adjust line; underset by %1%2",
+		     in_nroff_mode
+		     ? static_cast<int>(ceil(abs(space_deficit
+						 .to_units()
+					/ hresolution)))
+		     : abs(space_deficit.to_units() / warn_scale),
+		     in_nroff_mode ? 'n' : warn_scaling_unit);
     // The extra space is the amount of space to distribute among the
     // adjustable space nodes in an output line; this process occurs
     // only if adjustment is enabled.  We may however want to synthesize
