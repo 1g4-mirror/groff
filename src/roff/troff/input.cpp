1g4-mirror/groff

commit 6d32f2492ef3db455bd6306cdf7bd7c0a0e2d340

[troff]: Retype stream objects.
Make them objects of new `grostream` class, so we can conveniently store
more state in them.

* src/roff/troff/input.cpp (class grostream): Declare.

  (grostream::grostream): Declare constructor.

  (grostream::~grostream): Declare (trivial) destructor.

  (print_streams): Report mode in which the stream was opened.

  (open_file, close_stream, do_write_request, write_macro_request):
  Migrate to expect an object of type `grostream` from a
  `stream_dictionary.lookup()` instead of a pointer to `FILE`.

src/roff/troff/input.cpp
@@ -7215,17 +7215,40 @@ void terminal_continue()
   do_terminal(0, 1);
 }
 
+struct grostream {
+  string filename;
+  string mode;
+  FILE *file;
+  grostream(const string &fn, string m, FILE *fp);
+  ~grostream();
+};
+
+grostream::grostream(const string &fn, string m, FILE *fp)
+: filename(fn), mode(m), file(fp)
+{
+  filename += '\0'; // Don't leak garbage in print_streams().
+}
+
+// XXX: Maybe we should try to close the libc FILE stream here.
+// Resource Release Is Destruction?
+grostream::~grostream()
+{
+}
+
 object_dictionary stream_dictionary(20);
 
 void print_streams()
 {
   object_dictionary_iterator iter(stream_dictionary);
-  FILE *filestream;
-  symbol stream;
-  while (iter.get(&stream, (object **)&filestream)) {
-    assert(!stream.is_null());
-    if (stream != 0 /* nullptr */)
-      errprint("%1\n", stream.contents());
+  symbol stream_name;
+  grostream *grost;
+  while (iter.get(&stream_name, (object **)&grost)) {
+    assert(!stream_name.is_null());
+    if (stream_name != 0 /* nullptr */) {
+      errprint("%1\t", stream_name.contents());
+      errprint("%1\t%2\n", grost->filename.contents(),
+	       grost->mode.contents());
+    }
   }
   fflush(stderr);
   skip_line();
@@ -7235,8 +7258,10 @@ static void open_file(bool appending)
 {
   symbol stream = get_name(true /* required */);
   if (!stream.is_null()) {
-    symbol filename = get_long_name(true /* required */);
-    if (!filename.is_null()) {
+    symbol fnarg = get_long_name(true /* required */);
+    if (!fnarg.is_null()) {
+      const string mode = appending ? "appending" : "writing";
+      string filename = fnarg.contents();
       errno = 0;
       FILE *fp = fopen(filename.contents(), appending ? "a" : "w");
       if (0 /* nullptr */ == fp) {
@@ -7249,12 +7274,17 @@ static void open_file(bool appending)
 	stream_dictionary.remove(stream);
       }
       else {
-	FILE *oldfp = (FILE *)stream_dictionary.lookup(stream);
-	if (oldfp != 0 /* nullptr */ && (fclose(oldfp) != 0))
+	grostream *oldgrost
+	  = (grostream *)stream_dictionary.lookup(stream);
+	FILE *oldfp = oldgrost->file;
+	if (oldfp != 0 /* nullptr */ && (fclose(oldfp) != 0)) {
 	  error("cannot close file '%1' already associated with stream"
 		" '%2': %3", filename.contents(), strerror(errno));
-	else
-	  stream_dictionary.define(stream, (object *)fp);
+	  return;
+	}
+	grostream *grost = new grostream(filename.contents(), mode,
+					 &*fp);
+	stream_dictionary.define(stream, (object *)grost);
       }
     }
   }
@@ -7295,7 +7325,8 @@ static void opena_request() // .opena
 static void close_stream(symbol &stream)
 {
   assert(!stream.is_null());
-  FILE *fp = (FILE *)stream_dictionary.lookup(stream);
+  grostream *grost = (grostream *)stream_dictionary.lookup(stream);
+  FILE *fp = grost->file;
   if (0 /* nullptr */ == fp) {
     error("cannot close nonexistent stream '%1'", stream.contents());
     return;
@@ -7351,7 +7382,8 @@ void do_write_request(int newline)
     skip_line();
     return;
   }
-  FILE *fp = (FILE *)stream_dictionary.lookup(stream);
+  grostream *grost = (grostream *)stream_dictionary.lookup(stream);
+  FILE *fp = grost->file;
   if (0 /* nullptr */ == fp) {
     error("cannot write to nonexistent stream '%1'", stream.contents());
     skip_line();
@@ -7387,7 +7419,8 @@ void write_macro_request()
     skip_line();
     return;
   }
-  FILE *fp = (FILE *)stream_dictionary.lookup(stream);
+  grostream *grost = (grostream *)stream_dictionary.lookup(stream);
+  FILE *fp = grost->file;
   if (0 /* nullptr */ == fp) {
     error("no stream named '%1'", stream.contents());
     skip_line();

commit ec6e93c7368e823eaa8b44084730c9bbf4934a83

[troff]: Refactor `grostream` for memory safety.
* src/roff/troff/input.cpp: Refactor `grostream` class for memory
  safety.

  (struct grosteam): Convert from this...
  (class grostream): ...to this, inheriting from class `object` so that
  it casts less dangerously thence and thither when stashing
  `grostream`s into and retrieving them from `object_dictionary`
  objects.

  (class grostream): Also re-type `filename` and `mode` member variables
  from (groff) `string` to `symbol`, since they won't have embedded null
  characters (and take up half as much space; `sizeof (string)` is 16 on
  GNU/Linux amd64 whereas that of `symbol` is 8).  Making this change by
  itself exposed memory corruption problems, resolved by resituating
  `grostream` in the class hierarchy.

  (grostream::grostream): Drop now unnecessary explicit null
  termination.  That property is implied by the `symbol` class.

  (open_file): Make newly created object anonymous, and drop frightening
  C-style typecast.  Now a `grostream` casts safely and implicitly to
  and from `object`.

  Problem likely introduced by me in commit 6d32f2492, 24 September, but
  it was latent (requiring the `symbol` change noted above), and without
  JSON dumpers for `string` and `symbol` objects, visibility into memory
  mischief was limited if it didn't cause a SEGV on a developer's
  machine.

src/roff/troff/input.cpp
@@ -7605,20 +7605,18 @@ void terminal_continue()
   do_terminal(0, 1);
 }
 
-struct grostream {
-  string filename;
-  string mode;
+class grostream : public object {
+public:
+  symbol filename;
+  symbol mode;
   FILE *file;
-  grostream(const string &fn, string m, FILE *fp);
+  grostream(const char *fn, symbol m, FILE *fp);
   ~grostream();
 };
 
-grostream::grostream(const string &fn, string m, FILE *fp)
+grostream::grostream(const char *fn, symbol m, FILE *fp)
 : filename(fn), mode(m), file(fp)
 {
-  // Don't leak garbage in print_streams().
-  filename += '\0';
-  mode += '\0';
 }
 
 // XXX: Maybe we should try to close the libc FILE stream here.
@@ -7663,7 +7661,7 @@ static void open_file(bool appending)
   if (!stream.is_null()) {
     char *filename = read_rest_of_line_as_argument();
     if (filename != 0 /* nullptr */) {
-      const string mode = appending ? "appending" : "writing";
+      const char *mode = appending ? "appending" : "writing";
       errno = 0;
       FILE *fp = fopen(filename, appending ? "a" : "w");
       if (0 /* nullptr */ == fp) {
@@ -7686,8 +7684,8 @@ static void open_file(bool appending)
 	    return;
 	  }
 	}
-	grostream *grost = new grostream(filename, mode, &*fp);
-	stream_dictionary.define(stream, (object *)grost);
+	stream_dictionary.define(stream,
+				 new grostream(filename, mode, &*fp));
       }
     }
     // TODO: Add `filename` to file name set.