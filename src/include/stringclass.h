a8da07c16f646a446227699252550e3b4ef2a9a4
G. Branden Robinson <g.branden.robinson@gmail.com>
2025-04-22 22:02:49 -0500

    [libgroff]: Dump ancient compiler bug workaround.
    * src/include/stringclass.h (operator+): Dump workaround for ancient
      (~1995) compiler bug affecting ternary operator.  Sun C++ 4.0 is no
      longer relevant.
    
    Reverts the following groff 1.10 ChangeLog entry:
    
    Tue May 10 13:02:31 1994  James Clark  (jjc@jclark.com)
    
           * include/stringclass.h (operator +): Use ?: instead of `if' to
           work around Sun C++ 4.0 bug.

--- src/include/stringclass.h
+++ src/include/stringclass.h
@@ -138,26 +138,12 @@ inline string operator+(const string &s1, const string &s2)
 
 inline string operator+(const string &s1, const char *s2)
 {
-#ifdef __GNUG__
-  if (s2 == 0)
-    return s1;
-  else
-    return string(s1.ptr, s1.len, s2, strlen(s2));
-#else
   return s2 == 0 ? s1 : string(s1.ptr, s1.len, s2, strlen(s2));
-#endif
 }
 
 inline string operator+(const char *s1, const string &s2)
 {
-#ifdef __GNUG__
-  if (s1 == 0)
-    return s2;
-  else
-    return string(s1, strlen(s1), s2.ptr, s2.len);
-#else
   return s1 == 0 ? s2 : string(s1, strlen(s1), s2.ptr, s2.len);
-#endif
 }
 
 inline string operator+(const string &s, char c)
