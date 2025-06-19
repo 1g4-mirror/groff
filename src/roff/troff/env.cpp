1g4-mirror/groff

commit 8b6ccbce48d84ad40d50dcadc9fe60436e13fc1d

src/roff/troff/env.cpp: Fix code style nit.
* src/roff/troff/env.cpp (compare_ranges, distribute_space)
  (hyphen_trie::read_patterns_file): Favor C++-style `static_cast` and
  `const_cast` over C-style omnipotent casts.

src/roff/troff/env.cpp
@@ -234,7 +234,8 @@ extern "C" {
 
 int compare_ranges(const void *p1, const void *p2)
 {
-  return ((size_range *)p1)->min - ((size_range *)p2)->min;
+  return (  static_cast<size_range *>(const_cast<void *>(p1))->min
+	  - static_cast<size_range *>(const_cast<void *>(p2))->min);
 }
 
 }
@@ -2207,8 +2208,8 @@ static bool distribute_space(node *nd, int nspaces,
   if (!force_reverse_node_list && spread_limit >= 0
       && desired_space.to_units() > 0) {
     hunits em = curenv->get_size();
-    double Ems = (double)desired_space.to_units() / nspaces
-		 / (em.is_zero() ? hresolution : em.to_units());
+    double Ems = static_cast<double>(desired_space.to_units() / nspaces
+		 / (em.is_zero() ? hresolution : em.to_units()));
     if (Ems > spread_limit)
       output_warning(WARN_BREAK, "spreading %1m per space", Ems);
   }
@@ -4197,7 +4198,7 @@ void hyphen_trie::read_patterns_file(const char *name, int append,
     if (i > 0) {
       if (have_patterns || is_final_pattern || is_traditional) {
 	for (int j = 0; j < i; j++)
-	  buf[j] = hpf_code_table[(unsigned char)buf[j]];
+	  buf[j] = hpf_code_table[static_cast<unsigned char>(buf[j])];
 	insert_pattern(buf, i, num);
 	is_final_pattern = false;
       }

commit c9cbe4ba3cd3d44b7e1535fd5de987013fd302a6

[troff]: Fix Savannah #67219.
* src/roff/troff/env.cpp (distribute_space): Fix expression involving
  C++ `static_cast<>` operator to stop permitting integer division
  within, leading to undesired loss of precision when reporting the
  amount of "spread" applied when performing adjustment, as configured
  by the `spreadwarn` request.

  Fixes <https://savannah.gnu.org/bugs/?67219>.  [Problem introduced by
  me in commit 8b6ccbce48, 13 May.  --GBR]

src/roff/troff/env.cpp
@@ -2239,8 +2239,8 @@ static bool distribute_space(node *nd, int nspaces,
   if (!force_reverse_node_list && spread_limit >= 0
       && desired_space.to_units() > 0) {
     hunits em = curenv->get_size();
-    double Ems = static_cast<double>(desired_space.to_units() / nspaces
-		 / (em.is_zero() ? hresolution : em.to_units()));
+    double Ems = static_cast<double>(desired_space.to_units()) / nspaces
+		 / (em.is_zero() ? hresolution : em.to_units());
     if (Ems > spread_limit)
       output_warning(WARN_BREAK, "spreading %1m per space", Ems);
   }