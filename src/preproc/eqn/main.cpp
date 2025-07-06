milstedca/eqnx

commit d22a547b9bd2ed9f091c7feaac42696e68b7493f

src/preproc/eqn/main.cpp: Boolify.
* src/preproc/eqn/main.cpp (main): Trivially refactor; demote `int`
  `load_startup_file` to Boolean `want_startup_file`.

src/preproc/eqn/main.cpp
@@ -322,7 +322,7 @@ int main(int argc, char **argv)
   static char stderr_buf[BUFSIZ];
   setbuf(stderr, stderr_buf);
   int opt;
-  int load_startup_file = 1;
+  bool want_startup_file = true;
   static const struct option long_options[] = {
     { "help", no_argument, 0, CHAR_MAX + 1 },
     { "version", no_argument, 0, 'v' },
@@ -336,7 +336,7 @@ int main(int argc, char **argv)
       compatible_flag = 1;
       break;
     case 'R':			// don't load eqnrc
-      load_startup_file = 0;
+      want_startup_file = true;
       break;
     case 'M':
       config_macro_path.command_line_dir(optarg);
@@ -370,12 +370,12 @@ int main(int argc, char **argv)
       }
       else if (strcmp(device, "MathML") == 0) {
 	output_format = mathml;
-	load_startup_file = 0;
+	want_startup_file = false;
       }
       else if (strcmp(device, "mathml:xhtml") == 0) {
 	device = "MathML";
 	output_format = mathml;
-	load_startup_file = 0;
+	want_startup_file = false;
 	xhtml = 1;
       }
       break;
@@ -442,7 +442,7 @@ int main(int argc, char **argv)
 	   ".if !'%s'ps' .tm1 (consider invoking 'groff -Thtml -e')\n",
 	   device);
   }
-  if (load_startup_file) {
+  if (want_startup_file) {
     char *path;
     FILE *fp = config_macro_path.open_file(STARTUP_FILE, &path);
     if (fp) {

1g4-mirror/groff

commit 35afd373bf384d74c266be00c3d6457c96ab1a46

[eqn]: Actually honor `-R` option.
* src/preproc/eqn/main.cpp (main): Actually set `want_startup_file` to
  `false` when `-R` option is seen.  Problem introduced by me in commit
  d22a547b9b, 27 August 2023.

src/preproc/eqn/main.cpp
@@ -350,7 +350,7 @@ int main(int argc, char **argv)
       compatible_flag = 1;
       break;
     case 'R':			// don't load eqnrc
-      want_startup_file = true;
+      want_startup_file = false;
       break;
     case 'M':
       config_macro_path.command_line_dir(optarg);