e552775dd0b5f6233a1ec1b7765a7af41e3b2d0e
G. Branden Robinson <g.branden.robinson@gmail.com>
2025-07-14 18:36:48 -0500

    font/scripts/*:Prevent recurrence of prev problem.
    ...by computing the basename of the script file, storing it a variable
    `progname`, and using that variable in diagnostics.
    
    * font/scripts/gendesc.sh:
    * font/scripts/genfonts.sh: Do it.

--- font/scripts/gendesc.sh
+++ font/scripts/gendesc.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # Usage: gendesc.sh <input .proto> <RES> <CPI> <LPI> <list of fonts>
+progname=${0##*/}
 if test -z "$1" || test -z "$2" || test -z "$3" || test -z "$4" || \
     test -z "$5"; then
-    echo "gendesc.sh: missing parameter"; exit 255;
+    echo "$progname: missing parameter"; exit 255;
 fi
 INPUT=$1
 RES=$2
