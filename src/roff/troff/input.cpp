1g4-mirror/groff

commit 148b65b1ed45a51bac10689c36b557e1867341f5

[troff]: Make `pchar` report "Unicode mapping".
* src/roff/troff/input.cpp (charinfo::dump): Report Unicode mapping
  corresponding to character.

src/roff/troff/input.cpp
@@ -10434,6 +10434,17 @@ void charinfo::dump()
     }
     errprint(")\n");
   }
+  int mapping = get_unicode_mapping();
+  if (mapping >= 0) {
+    const size_t buflen = 6; // enough for five hex digits + '\0'
+    char hexbuf[buflen];
+    (void) memset(hexbuf, '\0', buflen);
+    (void) snprintf(hexbuf, buflen, "%.4X", mapping);
+    errprint("  Unicode mapping: U+%1\n", hexbuf);
+  }
+  else
+    errprint("  Unicode mapping: none (%1)\n", mapping);
+  errprint("  ASCII code: %1\n", static_cast<int>(ascii_code));
   errprint("  ASCII code: %1\n", static_cast<int>(ascii_code));
   errprint("  asciify code: %1\n", static_cast<int>(asciify_code));
   errprint("  is%1 found\n", is_not_found ? " not" : "");

commit 197fe2e535308c0592b7d67efab40f81e6f72ea1

[troff]: Drop duplicate line from `pchar` output.
* src/roff/troff/input.cpp (charinfo::dump): Drop duplicate line of
  output to the standard error stream introduced in commit 148b65b1ed,
  12 September.

src/roff/troff/input.cpp
@@ -10485,7 +10485,6 @@ void charinfo::dump()
   else
     errprint("  Unicode mapping: none (%1)\n", mapping);
   errprint("  ASCII code: %1\n", static_cast<int>(ascii_code));
-  errprint("  ASCII code: %1\n", static_cast<int>(ascii_code));
   errprint("  asciify code: %1\n", static_cast<int>(asciify_code));
   errprint("  is%1 found\n", is_not_found ? " not" : "");
   errprint("  is%1 transparently translatable\n",