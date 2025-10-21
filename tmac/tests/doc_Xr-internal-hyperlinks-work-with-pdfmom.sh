0a71eba831d8e22b8fbac562d1cfab19992e61de
G. Branden Robinson <g.branden.robinson@gmail.com>
2025-10-21 14:28:35 -0500

    [gropdf]: Add unit tests.
    ...for pdfmom(1)'s replacement of external man page reference hyperlinks
    with internal links to document bookmarks.
    
    * tmac/tests/an_MR-internal-hyperlinks-work-with-pdfmom.sh:
    * tmac/tests/andoc_internal-hyperlinks-work-with-pdfmom.sh:
    * tmac/tests/doc_Xr-internal-hyperlinks-work-with-pdfmom.sh: Do it.
    
    * tmac/tmac.am (tmac_TESTS): Run tests.

---
+++ tmac/tests/doc_Xr-internal-hyperlinks-work-with-pdfmom.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+#
+# Copyright 2025 G. Branden Robinson
+#
+# This file is part of groff.
+#
+# groff is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# groff is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+pdfmom="${abs_top_builddir:-.}/pdfmom"
+groff="${abs_top_builddir:-.}/test-groff"
+
+fail=
+
+wail () {
+    echo ...FAILED >&2
+    fail=yes
+}
+
+# We test backward links first because that is the more robust
+# operation; it doesn't require two-pass formatting.
+
+input='.
+.Dd 2025-10-21
+.Dt foo 1
+.Os "groff test suite"
+.Sh Name
+.Nm foo
+.Nd a command with a very short name
+.Sh Description
+The real work is done by
+.Xr bar 1 .
+.Dd 2025-10-21
+.Dt bar 1
+.Os "groff test suite"
+.Sh Name
+.Nm bar
+.Nd a command that is difficult to use
+.Sh Description
+Novice users should avoid this command in favor of its wrapper,
+.Xr foo 1 .
+.'
+
+output=$(echo "$input" \
+  | GROFF_COMMAND="$groff" "$pdfmom" --roff -rU1 -mdoc -Z | nl -ba)
+echo "$output"
+
+# Expected (lines truncated):
+#   90	x X pdf: markstart 6830 -1770 2000 /Subtype /Link /Dest /bar(1)
+#  222	x X pdf: markstart 6830 -1770 2000 /Subtype /Link /Dest /foo(1)
+
+echo "checking that backward internal links work" >&2
+echo "$output" \
+  | grep -Eq '^ *222[[:space:]]+x X pdf: markstart .*/Dest /foo\(1\)' \
+  || wail
+
+echo "checking that forward internal links work" >&2
+echo "$output" \
+  | grep -Eq '^ *90[[:space:]]+x X pdf: markstart .*/Dest /bar\(1\)' \
+  || wail
+
+test -z "$fail"
+
+# vim:set autoindent expandtab shiftwidth=2 tabstop=2 textwidth=72:
