5707f86c2dfdfdb22b23f3d62ae36f03b6b57f7c
G. Branden Robinson <g.branden.robinson@gmail.com>
2025-10-30 04:01:03 -0500

    [grotty]: Revise copyright notice in test script.
    Identify myself as the copyright holder.  I've never executed copyright
    assignment paperwork with the FSF, so my "legally significant"[1]
    origination of this file in 2022 can't be under the FSF's copyright.
    
    Drop ersatz '(C)' copyright symbol.  Software developers have long
    labored under the no-longer-correct misconception that omitting a
    copyright symbol from one's notice was a fatal defect that effectively
    placed the work in the public domain.  That stopped being true as of 1
    March 1989.[2]  Further, prior to guidance issued by the U.S. Copyright
    Office in the decades since, the use of "(C)" as a substitute for a
    copyright sign _may not have sufficed_ to prevent the copyright notice
    from being regarded as defective.  The Copyright Office, then and now,
    prefers the abbreviation "copr." when © is typographically
    unavailable.[ibid.]  Nowadays, its advice is that "c" (note lowercase)
    is an "acceptable variant", that _may_ retain the efficacy of the
    copyright notice.  The word "copyright", spelled out in full, also
    suffices per that resource, and is already present in this notice.
    
    Drop stump comment after license information.
    
    Update Vim modeline for human comprehension.
    
    [1] "A change of just a few lines (less than 15 or so) is not legally
    significant for copyright."  Conversely, >= 15 lines would be.
    
    https://www.gnu.org/prep/maintain/html_node/Legally-Significant.html
    
    [2] https://www.copyright.gov/circs/circ03.pdf

--- src/devices/grotty/tests/basic_latin_glyphs_map_correctly.sh
+++ src/devices/grotty/tests/basic_latin_glyphs_map_correctly.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2022-2024 Free Software Foundation, Inc.
+# Copyright 2022-2024 G. Branden Robinson
 #
 # This file is part of groff.
 #
@@ -16,7 +16,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
 
 grotty="${abs_top_builddir:-.}/grotty"
 
@@ -211,4 +210,4 @@ done
 
 test -z "$fail"
 
-# vim:set ai et sw=4 ts=4 tw=72:
+# vim:set autoindent expandtab shiftwidth=4 tabstop=4 textwidth=72:
