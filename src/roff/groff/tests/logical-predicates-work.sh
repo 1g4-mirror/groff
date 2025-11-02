1g4-mirror/groff

commit fb6fdbd9d174262ab57572bf095100459b589a66

[troff]: Allow more delimiters in compat mode.
* src/roff/troff/input.cpp (token::is_usable_as_delimiter): If in
  compatibility mode, accept any ordinary character as a delimiter.

* doc/groff.texi.in (Delimiters, Compatibility Mode):
* man/groff_diff.7.man (Compatibility mode): Document it.

* src/roff/groff/tests/check-delimiter-validity.sh: Test it.
* src/roff/groff/groff.am (groff_TESTS): Run test.

* NEWS: Add item.

src/roff/groff/groff.am
@@ -44,6 +44,7 @@ groff_TESTS = \
   src/roff/groff/tests/backslash-s-works-with-single-digit-argument.sh \
   src/roff/groff/tests/break-zero-length-output-line-sanely.sh \
   src/roff/groff/tests/cf-request-early-does-not-fail.sh \
+  src/roff/groff/tests/check-delimiter-validity.sh \
   src/roff/groff/tests/current-language-and-environment-in-sync.sh \
   src/roff/groff/tests/degenerate-control-flow-works.sh \
   src/roff/groff/tests/detect-evil-link-time-optimizer.sh \

doc/groff.texi.in
@@ -7304,10 +7304,14 @@ the numerals @code{0}-@code{9} and the decimal point @code{.}
 @ifinfo
 @cindex <colon>, as delimiter
 @end ifinfo
-@cindex @code{|}, as delimiter
+@c @cindex @code{|}, as delimiter
 @cindex @code{(}, as delimiter
 @cindex @code{)}, as delimiter
-the (single-character) operators @samp{+-/*%<>=&:()|}
+the (single-character) operators @samp{+-/*%<>=&:()}@footnote{GNU
+@command{troff} accepts @samp{|} as a delimiter in spite of its
+meaningfulness in numeric expressions because it occasionally sees use
+in man pages.  Future @code{groff} releases may deprecate and
+subsequently withdraw such support.}
 
 @item
 @cindex space character, as delimiter
@@ -17759,6 +17763,40 @@ at the end.  As in the C language, we all have to share one big
 name space, so choose a register name that is unlikely to collide with
 other uses.
 @endDefreq
+
+@cindex additional delimiters accepted by @acronym{AT&T} @code{troff}
+@cindex delimiters, additional, accepted by @acronym{AT&T} @code{troff}
+In compatibility mode,
+GNU
+@command{troff}
+accepts several characters as delimiters that it ordinarily rejects,
+because they are meaningful in numeric expressions and therefore
+potentially ambiguous to the document maintainer.
+The set of additional delimiters is
+@samp{0}
+@samp{1}
+@samp{2}
+@samp{3}
+@samp{4}
+@samp{5}
+@samp{6}
+@samp{7}
+@samp{8}
+@samp{9}
+@samp{+}
+@samp{-}
+@samp{/}
+@samp{*}
+@samp{%}
+@samp{<}
+@samp{>}
+@samp{=}
+@samp{&}
+@samp{:}
+@samp{(}
+@samp{)},
+and
+@samp{.}.
 
 @cindex input level in delimited arguments
 @cindex interpolation depth in delimited arguments

man/groff_diff.7.man
@@ -5692,6 +5692,41 @@ turns compatibility mode
 while it interprets its argument list.
 .
 .
+.P
+In compatibility mode,
+GNU
+.\" troff \" GNU
+accepts several characters as delimiters that it ordinarily rejects,
+because they are meaningful in numeric expressions and therefore
+potentially ambiguous to the document maintainer.
+.
+The set of additional delimiters is
+\[lq]0\[rq]
+\[lq]1\[rq]
+\[lq]2\[rq]
+\[lq]3\[rq]
+\[lq]4\[rq]
+\[lq]5\[rq]
+\[lq]6\[rq]
+\[lq]7\[rq]
+\[lq]8\[rq]
+\[lq]9\[rq]
+\[lq]+\[rq]
+\[lq]\-\[rq]
+\[lq]/\[rq]
+\[lq]*\[rq]
+\[lq]%\[rq]
+\[lq]<\[rq]
+\[lq]>\[rq]
+\[lq]=\[rq]
+\[lq]&\[rq]
+\[lq]:\[rq]
+\[lq](\[rq]
+\[lq])\[rq],
+and
+\[lq].\[rq].
+.
+.
 .\" ====================================================================
 .SH "Other differences"
 .\" ====================================================================

src/roff/groff/tests/check-delimiter-validity.sh
@@ -1,0 +1,66 @@
+#!/bin/sh
+#
+# Copyright (C) 2024 Free Software Foundation, Inc.
+#
+# This file is part of groff.
+#
+# groff is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# groff is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+groff="${abs_top_builddir:-.}/test-groff"
+
+fail=
+
+wail () {
+  echo ...FAILED >&2
+  fail=YES
+}
+
+for c in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \
+         a b c d e f g h i j k l m n o p q r s t u v w x y z '|'
+do
+  echo "checking validity of '$c' as delimiter in normal mode" \
+    >&2
+  output=$(printf '\\l%c1n+2n\\&_%c\n' "$c" "$c" \
+    | "$groff" -T ascii | sed '/^$/d')
+  echo "$output"
+  echo "$output" | grep -Fqx ___ || wail
+done
+
+for c in 0 1 2 3 4 5 6 7 8 9 + - / '*' % '<' '>' = '&' : '(' ')' .
+do
+  echo "checking invalidity of '$c' as delimiter in normal mode" \
+    >&2
+  output=$(printf '\\l%c1n+2n\\&_%c\n' "$c" "$c" \
+    | "$groff" -T ascii | sed '/^$/d')
+  echo "$output"
+  echo "$output" | grep -qx 1n+2n_. || wail
+done
+
+# All of these work in DWB nroff.
+for c in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \
+         a b c d e f g h i j k l m n o p q r s t u v w x y z \
+         0 1 2 3 4 5 6 7 8 9 + - / '*' % '<' '>' = '&' : '(' ')' . '|'
+do
+  echo "checking validity of '$c' as delimiter in $mode mode" \
+    >&2
+  output=$(printf '\\l%c1n+2n\\&_%c\n' "$c" "$c" \
+    | "$groff" -C -T ascii | sed '/^$/d')
+  echo "$output"
+  echo "$output" | grep -Fqx ___ || wail
+done
+
+test -z "$fail"
+
+# vim:set autoindent expandtab shiftwidth=2 tabstop=2 textwidth=72:

src/roff/troff/input.cpp
@@ -2585,17 +2585,9 @@ bool token::operator!=(const token &t)
 // doesn't tokenize it) and accepts a user-specified delimiter.
 static bool is_char_usable_as_delimiter(int c)
 {
+  if (csdigit(c))
+    return false;
   switch (c) {
-  case '0':
-  case '1':
-  case '2':
-  case '3':
-  case '4':
-  case '5':
-  case '6':
-  case '7':
-  case '8':
-  case '9':
   case '+':
   case '-':
   case '/':
@@ -2626,6 +2618,10 @@ bool token::is_usable_as_delimiter(bool report_error)
   bool is_valid = false;
   switch (type) {
   case TOKEN_CHAR:
+    // AT&T troff accepted any character as a delimiter, even perverse
+    // choices like `\l91n+2n\&*`.  See Savannah #66481.
+    if (want_att_compat)
+      return true;
     is_valid = is_char_usable_as_delimiter(c);
     if (!is_valid && report_error)
       error("character '%1' is not allowed as a delimiter",

commit 5645673e534ac879581df0f8e1de17cdbb08f17a

[groff]: Unit-test logical predicates.
* src/roff/groff/tests/logical-predicates-work.sh: Test behavior of
  logical predicates (the first argument to `if`, `ie`, and `while`
  requests).

* src/roff/groff/groff.am (groff_TESTS): Run test.

Test fails at this commit, exposing a regression I introduced in commit
fb6fdbd9d1, 2024-11-06.

src/roff/groff/groff.am
@@ -92,6 +92,7 @@ groff_TESTS = \
   src/roff/groff/tests/initialization-is-quiet.sh \
   src/roff/groff/tests/latin1-device-maps-oq-to-0x27.sh \
   src/roff/groff/tests/lf-request-works.sh \
+  src/roff/groff/tests/logical-predicates-work.sh \
   src/roff/groff/tests/localization-works.sh \
   src/roff/groff/tests/msoquiet-request-works.sh \
   src/roff/groff/tests/output-request-works.sh \

src/roff/groff/tests/logical-predicates-work.sh
@@ -1,0 +1,128 @@
+#!/bin/sh
+#
+# Copyright 2025 G. Branden Robinson
+#
+# This file is part of groff.
+#
+# groff is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# groff is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+groff="${abs_top_builddir:-.}/test-groff"
+
+fail=
+
+wail () {
+  echo ...FAILED >&2
+  fail=YES
+}
+
+input='.
+.ec @
+.@" Construct cases expecting "true"; invalid syntax is "false".
+.ie t  .tm case "t"  true
+.el    .tm case "t"  FALSE
+.ie !n .tm case "!n" true
+.el    .tm case "!n" FALSE
+@c
+.ie o  .tm case "o"  true
+.el    .tm case "o"  FALSE
+.ie !e .tm case "!e" true
+.el    .tm case "!e" FALSE
+.
+.@" GNU troff extensions
+.ie c a       .tm case "c"  true
+.el           .tm case "c"  FALSE
+.ie d .T      .tm case "d"  true
+.el           .tm case "d"  FALSE
+.ie F TR      .tm case "F"  true
+.el           .tm case "F"  FALSE
+.ie m default .tm case "m"  true
+.el           .tm case "m"  FALSE
+.ie r .T      .tm case "r"  true
+.el           .tm case "r"  FALSE
+.ie S R       .tm case "S"  true
+.el           .tm case "S"  FALSE
+.ie !v        .tm case "!v" true
+.el           .tm case "!v" FALSE
+.
+.@" numeric expressions
+.ie 1      .tm case "1"  true
+.el        .tm case "1"  FALSE
+.ie !0     .tm case "!0" true
+.el        .tm case "!0" FALSE
+.ie @n(.T  .tm register interpolation case true
+.el        .tm register interpolation case FALSE
+.ie !@n(.k .tm negated register interpolation case true
+.el        .tm negated register interpolation case FALSE
+.
+.@" output comparison
+.if @n(.C .tm expect warnings
+.ie /foo/foo/  .tm output comparison case true
+.el            .tm output comparison case FALSE
+.ie !/foo/bar/ .tm negated output comparison case true
+.el            .tm negated output comparison case FALSE
+.'
+
+error=$(printf "%s\n" "$input" | "$groff" -ww -T ps 2>&1 >/dev/null)
+echo "$error"
+
+echo "$error" | grep -Fqx 'case "t"  true' || wail
+echo "$error" | grep -Fqx 'case "!n" true' || wail
+echo "$error" | grep -Fqx 'case "o"  true' || wail
+echo "$error" | grep -Fqx 'case "!e" true' || wail
+echo "$error" | grep -Fqx 'case "c"  true' || wail
+echo "$error" | grep -Fqx 'case "d"  true' || wail
+echo "$error" | grep -Fqx 'case "m"  true' || wail
+echo "$error" | grep -Fqx 'case "r"  true' || wail
+echo "$error" | grep -Fqx 'case "S"  true' || wail
+echo "$error" | grep -Fqx 'case "!v" true' || wail
+echo "$error" | grep -Fqx 'case "1"  true' || wail
+echo "$error" | grep -Fqx 'case "!0" true' || wail
+echo "$error" | grep -Fqx 'register interpolation case true' \
+    || wail
+echo "$error" | grep -Fqx 'negated register interpolation case true' \
+    || wail
+echo "$error" | grep -Fqx 'output comparison case true' \
+    || wail
+echo "$error" | grep -Fqx 'negated output comparison case true' \
+    || wail
+
+echo '********** checking compatibility mode behavior' >&2
+
+error=$(printf "%s\n" "$input" | "$groff" -C -ww -T ps 2>&1 >/dev/null)
+echo "$error"
+
+echo "$error" | grep -Fqx 'case "t"  true' || wail
+echo "$error" | grep -Fqx 'case "!n" true' || wail
+echo "$error" | grep -Fqx 'case "o"  true' || wail
+echo "$error" | grep -Fqx 'case "!e" true' || wail
+echo "$error" | grep -Fqx 'case "c"  true' || wail
+echo "$error" | grep -Fqx 'case "d"  true' || wail
+echo "$error" | grep -Fqx 'case "m"  true' || wail
+echo "$error" | grep -Fqx 'case "r"  true' || wail
+echo "$error" | grep -Fqx 'case "S"  true' || wail
+echo "$error" | grep -Fqx 'case "!v" true' || wail
+echo "$error" | grep -Fqx 'case "1"  true' || wail
+echo "$error" | grep -Fqx 'case "!0" true' || wail
+echo "$error" | grep -Fqx 'register interpolation case true' \
+    || wail
+echo "$error" | grep -Fqx 'negated register interpolation case true' \
+    || wail
+echo "$error" | grep -Fqx 'output comparison case FALSE' \
+    || wail
+echo "$error" | grep -Fqx 'negated output comparison case FALSE' \
+    || wail
+
+test -z "$fail"
+
+# vim:set autoindent expandtab shiftwidth=4 tabstop=4 textwidth=72: