55d6898aaeb8cf71f41c31615bbd94a17934aeb2
G. Branden Robinson <g.branden.robinson@gmail.com>
2025-11-06 03:10:01 -0600

    [troff]: Test more compatibility mode delimiters.
    * src/roff/groff/tests/check-delimiter-validity.sh: Test many more
      string expression delimiters in compatibility mode, including
      numerals and punctuation characters.

--- src/roff/groff/tests/check-delimiter-validity.sh
+++ src/roff/groff/tests/check-delimiter-validity.sh
@@ -59,9 +59,12 @@ done
 
 # Now test the context-dependent sets of delimiters of AT&T troff.
 
+# not tested: '_' (because it's part of our delimited expression)
 for c in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \
          a b c d e f g h i j k l m n o p q r s t u v w x y z \
-         0 1 2 3 4 5 6 7 8 9 + - / '*' % '<' '>' = '&' : '(' ')' . '|'
+         0 1 2 3 4 5 6 7 8 9 + - / '*' % '<' '>' = '&' : '(' ')' . '|' \
+         '!' '"' '#' '$' "'" ',' ';' '?' \
+         '@' '[' ']' '^' '`' '{' '}' '~'
 do
     echo "checking validity of '$c' as string expression delimiter" \
          "in compatibility mode" >&2
