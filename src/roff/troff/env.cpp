1g4-mirror/groff

commit 8b6ccbce48d84ad40d50dcadc9fe60436e13fc1d

src/roff/troff/env.cpp: Fix code style nit.
* src/roff/troff/env.cpp (compare_ranges, distribute_space)
  (hyphen_trie::read_patterns_file): Favor C++-style `static_cast` and
  `const_cast` over C-style omnipotent casts.

src/roff/troff/env.cpp
@@ -234,7 +234,8 @@ extern "C" {
 
 int compare_ranges(const void *p1, const void *p2)
 {
-  return ((size_range *)p1)->min - ((size_range *)p2)->min;
+  return (  static_cast<size_range *>(const_cast<void *>(p1))->min
+	  - static_cast<size_range *>(const_cast<void *>(p2))->min);
 }
 
 }
@@ -2207,8 +2208,8 @@ static bool distribute_space(node *nd, int nspaces,
   if (!force_reverse_node_list && spread_limit >= 0
       && desired_space.to_units() > 0) {
     hunits em = curenv->get_size();
-    double Ems = (double)desired_space.to_units() / nspaces
-		 / (em.is_zero() ? hresolution : em.to_units());
+    double Ems = static_cast<double>(desired_space.to_units() / nspaces
+		 / (em.is_zero() ? hresolution : em.to_units()));
     if (Ems > spread_limit)
       output_warning(WARN_BREAK, "spreading %1m per space", Ems);
   }
@@ -4197,7 +4198,7 @@ void hyphen_trie::read_patterns_file(const char *name, int append,
     if (i > 0) {
       if (have_patterns || is_final_pattern || is_traditional) {
 	for (int j = 0; j < i; j++)
-	  buf[j] = hpf_code_table[(unsigned char)buf[j]];
+	  buf[j] = hpf_code_table[static_cast<unsigned char>(buf[j])];
 	insert_pattern(buf, i, num);
 	is_final_pattern = false;
       }

commit 9fd4ff04f0323a82ccefb128aef730da299492c3

src/roff/troff/env.cpp: Revise copyright notice.
Add copyright notice for myself.  I have never executed copyright
assignment paperwork with the FSF, so my "legally significant"[1]
changes to this file[2] can't be under the FSF's copyright.

(I retain an FSF copyright notice corresponding to all changes not
authored by me--and which are above the "legal significance"
threshold--because I don't know who _has_ filed copyright assignment
paperwork with the FSF; I know only that I [and Ingo Schwarze] haven't.)

Justification:

$ git diff --stat 62601add468b2eff56910173e339e3fa4490a3de^ HEAD -- src/roff/troff/env.cpp
 src/roff/troff/env.cpp | 2852 +++++++++++++++++++++++-----------------
 1 file changed, 1680 insertions(+), 1172 deletions(-)

(I authored all commits to this file since 62601add46 except for the
following.

commit c9cbe4ba3cd3d44b7e1535fd5de987013fd302a6
Author: Dave Kemper <saint.snit@gmail.com>
Date:   Mon Jun 16 05:56:45 2025 +0000

    [troff]: Fix Savannah #67219.

    * src/roff/troff/env.cpp (distribute_space): Fix expression involving
      C++ `static_cast<>` operator to stop permitting integer division
      within, leading to undesired loss of precision when reporting the
      amount of "spread" applied when performing adjustment, as configured
      by the `spreadwarn` request.

      Fixes <https://savannah.gnu.org/bugs/?67219>.  [Problem introduced by
      me in commit 8b6ccbce48, 13 May.  --GBR]

 src/roff/troff/env.cpp | 4 ++--
 1 file changed, 2 insertions(+), 2 deletions(-)
)

[1] "A change of just a few lines (less than 15 or so) is not legally
significant for copyright."  Conversely, >= 15 lines would be.

https://www.gnu.org/prep/maintain/html_node/Legally-Significant.html

[2] The history of this file supplies an interesting test case for the
    "legal significance" threshold if we disregard cumulativity and
    apply it only to individual commits.  (It's not clear to me which

src/roff/troff/env.cpp
@@ -1,4 +1,6 @@
 /* Copyright 1989-2025 Free Software Foundation, Inc.
+             2020-2025 G. Branden Robinson
+
      Written by James Clark (jjc@jclark.com)
 
 This file is part of groff.