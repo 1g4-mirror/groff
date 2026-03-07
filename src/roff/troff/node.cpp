b6a75659ab17651593e6a38464a9b8444c6f2a94
G. Branden Robinson <g.branden.robinson@gmail.com>
2026-03-07 04:53:22 -0600

    src/roff/troff/node.cpp: Revise copyright notice.
    Ratchet back ending year of FSF copyright notice to 2010, reflecting the
    date of the last "legally significant"[1] contribution to this file by
    anyone but me, which was commit 18dae4eaed, 2010-12-20, by Werner
    Lemberg.
    
    (The aforementioned is one of those changes that on superficial analysis
    doesn't look "legally significant", because its net contribution of code
    to this file is negative 209 lines.  Proper analysis is often not
    superficial.)
    
    [1] See groff's "HACKING" file, section "Updating Copyright Notices".

--- src/roff/troff/node.cpp
+++ src/roff/troff/node.cpp
@@ -1,4 +1,4 @@
-/* Copyright 1989-2020 Free Software Foundation, Inc.
+/* Copyright 1989-2010 Free Software Foundation, Inc.
              2021-2025 G. Branden Robinson
 
 Written by James Clark (jjc@jclark.com)
