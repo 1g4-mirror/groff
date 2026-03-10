1g4-mirror/groff

commit 52025b06f50b485ce44647298bccd9ac0e38dde4

src/roff/troff/input.cpp: Add assertions (8/10).
* src/roff/troff/input.cpp (do_translate): Add assertions.

src/roff/troff/input.cpp
@@ -8375,8 +8375,11 @@ static void do_translate(bool transparently, bool as_input)
       continue;
     }
     charinfo *ci1 = tok.get_charinfo(true /* required */);
-    if (0 /* nullptr */ == ci1)
+    if (0 /* nullptr */ == ci1) {
+      assert(0 == "attempted to use token without charinfo in title"
+	     " formatting request");
       break;
+    }
     tok.next();
     if (tok.is_newline() || tok.is_eof()) {
       ci1->set_special_translation(charinfo::TRANSLATE_SPACE,
@@ -8397,8 +8400,11 @@ static void do_translate(bool transparently, bool as_input)
 				   transparently);
     else {
       charinfo *ci2 = tok.get_charinfo(true /* required */);
-      if (0 /* nullptr */ == ci2)
+      if (0 /* nullptr */ == ci2) {
+	assert(0 == "attempted to use token without charinfo in"
+	       " character translation request");
 	break;
+      }
       if (ci1 == ci2)
 	ci1->set_translation(0 /* nullptr */, transparently, as_input);
       else

commit 9c4e2c8c6a26ebfb03865fb05dc121c77a553936

[troff]: Fix Savannah #68132.
* src/roff/troff/input.cpp (do_translate): Stop throwing assertions if
  one of the participants (source or destination) in a character
  translation is a non-character.  Historical documents like Ritchie's
  "C Reference Manual" from Sixth Edition Unix (1975) did things like
  `.tr ~\|`.  This is no longer a worthwhile idiom--if indeed it ever
  was, since strings were available even then, but especially not since
  1990 or so with GNU troff's `char` and related requests--but we should
  not abort the formatter on encountering them.  Actually _supporting_
  such translations, in compatibility mode only, is under consideration
  for groff 1.25.  Throw error when encountering `tr` requests that
  attempt to operate on non-characters outside of compatibility mode.

Fixes <https://savannah.gnu.org/bugs/?68132>.  Thanks to Clem Cole for
the report.  Problem introduced by me in 52025b06f5, 21 November, in the
course of adding many assertions to GNU troff to try to smoke out
transitions into invalid formatter state and code that should be
unreachable but isn't.  This instance fooled me because we did nothing
useful (nor consistent with AT&T troff behavior) with the non-character
translation participant; see above regarding future plans.

ANNOUNCE: Acknowledge Clem.

ANNOUNCE
@@ -208,5 +208,6 @@ of this release.
    similar from the contrib/ change log files may also be necessary. ]]
 
 Bjarni Ingi Gislason
+Clem Cole
 Peng Zhang
 Rocket Ma

src/roff/troff/input.cpp
@@ -8594,10 +8594,19 @@ static void do_translate(bool transparently, bool as_input)
       tok.next();
       continue;
     }
+    // The semantics of character translation operations on non-
+    // characters (space escape sequences) are ill-defined and vary
+    // among troffs; see Savannah #68133.
+    //
+    // TODO: However, documents such as Ritchie's "C Reference Manual"
+    // distributed with Sixth Edition Unix did things like `.tr ^\|`, so
+    // some support should be added, only for those idioms, and only in
+    // compatibility mode.
     charinfo *ci1 = tok.get_charinfo(true /* required */);
     if (0 /* nullptr */ == ci1) {
-      assert(0 == "attempted to use token without charinfo in character"
-	     " translation request");
+      if (!want_att_compat)
+	error("cannot perform character translation from %1",
+	      tok.description());
       break;
     }
     tok.next();
@@ -8621,8 +8630,9 @@ static void do_translate(bool transparently, bool as_input)
     else {
       charinfo *ci2 = tok.get_charinfo(true /* required */);
       if (0 /* nullptr */ == ci2) {
-	assert(0 == "attempted to use token without charinfo in"
-	       " character translation request");
+	if (!want_att_compat)
+	  error("cannot perform character translation to %1",
+		tok.description());
 	break;
       }
       if (ci1 == ci2)