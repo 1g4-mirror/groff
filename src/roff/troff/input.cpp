1ae18d1ee0b3c6b86cb1d078f57ac33beba25cf7
G. Branden Robinson <g.branden.robinson@gmail.com>
2026-03-20 18:41:24 -0500

    src/roff/troff/input.cpp: Update copyright.
    Justification (year, commit ID, summary, net lines added by commit):
    
    2026 0220f00c3c [troff]: Report category in warning diagnostics. (+15)
    
    See groff's "HACKING" file, section "Updating Copyright Notices".

--- src/roff/troff/input.cpp
+++ src/roff/troff/input.cpp
@@ -1,5 +1,5 @@
 /* Copyright 1989-2024 Free Software Foundation, Inc.
-             2021-2025 G. Branden Robinson
+             2021-2026 G. Branden Robinson
 
 Written by James Clark (jjc@jclark.com)
 
