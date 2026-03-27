f5ecb39a03043116d5dbdf255d790fd0c6805542
G. Branden Robinson <g.branden.robinson@gmail.com>
2026-03-27 18:57:43 -0500

    [libdriver]: Cosmetically revise license texts.
    * src/libs/libdriver/input.cpp:
    * src/libs/libdriver/printer.cpp: Refill GPLv3 license notice at 72
      columns, like other groff files.

--- src/libs/libdriver/input.cpp
+++ src/libs/libdriver/input.cpp
@@ -3,20 +3,20 @@
 Written by James Clark (jjc@jclark.com)
 Major rewrite 2001 by Bernd Warken <groff-bernd.warken-72@web.de>
 
-   This file is part of groff, the GNU roff text processing system.
+This file is part of groff, the GNU roff text processing system.
 
-   groff is free software; you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+groff is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your
+option) any later version.
 
-   groff is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
+groff is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /* Description
