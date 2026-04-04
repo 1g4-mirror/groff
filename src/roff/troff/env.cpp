1g4-mirror/groff

commit e05fb6b37773cf260e962ece11a98a4da495962f

[troff]: Fix `phw` SEGV when no hyphenation lang.
* src/roff/troff/env.cpp (print_hyphenation_exceptions): Ensure that a
  current hyphenation language is defined before attempting to iterate
  its exception dictionary.

Fixes:
  $ ./build/test-groff
  .hla
  .phw
  groff: error: troff: Segmentation fault (core dumped)

Problem introduced by me in commit 0b40885e71, 3 November 2023.

src/roff/troff/env.cpp
@@ -3817,6 +3817,8 @@ static void add_hyphenation_exceptions()
 
 static void print_hyphenation_exceptions()
 {
+  if (0 /* nullptr */ == current_language)
+    return;
   dictionary_iterator iter(current_language->exceptions);
   symbol entry;
   unsigned char *hypoint;

commit e470a410a13a20c92974214da7eeb9db8f206344

[troff]: Fix Savannah #68203.
* src/roff/troff/env.cpp (print_hyphenation_exceptions): Skip the
  remainder of the input line if no hyphenation language is configured.

Fixes <https://savannah.gnu.org/bugs/?68203>.  Problem introduced by me
in commit e05fb6b377, 2024-11-07 (itself a fix for a SEGV under the same
circumstance).

src/roff/troff/env.cpp
@@ -3953,8 +3953,10 @@ static void add_hyphenation_exception_words_request()
 
 static void print_hyphenation_exceptions()
 {
-  if (0 /* nullptr */ == current_language)
+  if (0 /* nullptr */ == current_language) {
+    skip_line();
     return;
+  }
   dictionary_iterator iter(current_language->exceptions);
   symbol entry;
   unsigned char *hypoint;