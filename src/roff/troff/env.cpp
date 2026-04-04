1g4-mirror/groff

commit effd6835dc479834832640c37cd06b1cc1e0ee67

[troff]: Eliminate an error diagnostic.
* src/roff/troff/env.cpp (add_hyphenation_exceptions): Drop `error()`
  call prior to `assert()`.  It's okay if, when built with the
  preprocessor `NDEBUG` symbol defined, we silently discard the
  remainder of the input line and resume reading the next.  Should feel
  familiar to users of AT&T troff.  (Also, this makes a forthcoming
  change easier.)

src/roff/troff/env.cpp
@@ -3849,9 +3849,8 @@ static void add_hyphenation_exceptions()
 	   && !tok.is_eof()) {
       charinfo *ci = tok.get_charinfo(true /* required */);
       if (0 /* nullptr */ == ci) {
-	error("%1 has no associated character information(!)",
-	      tok.description());
-	assert(0 == "unexpected null pointer to charinfo of token");
+	assert(0 == "attempted to use token without charinfo in"
+	       " hyphenation exception word");
 	skip_line();
 	return;
       }

commit 29765f6d65eb06acaecd6615a85c6a4f8accd497

[troff]: Fix Savannah #68202 (1/2).
* src/roff/troff/env.cpp (add_hyphenation_exception_words_request): Stop
  throwing assert(3)ion if an argument contains a non-character.  The
  assertion is invalidated by plausible user input like:
    `.hw baz\%qux`
  and similar.

Begins fixing <https://savannah.gnu.org/bugs/?68202>.  Problem
introduced by me in commit effd6835dc, 19 November.

src/roff/troff/env.cpp
@@ -3904,8 +3904,6 @@ static void add_hyphenation_exception_words_request()
 	   && !tok.is_eof()) {
       charinfo *ci = tok.get_charinfo(true /* required */);
       if (0 /* nullptr */ == ci) {
-	assert(0 == "attempted to use token without charinfo in"
-	       " hyphenation exception word");
 	skip_line();
 	return;
       }