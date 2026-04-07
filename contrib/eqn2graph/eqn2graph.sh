1g4-mirror/groff

commit c34c6cc58cfb03f3cdb614b9e2dffb8da4cce924

[nroff]: Improve shell behavior check.
...and fix goof in test script.

* src/roff/nroff/nroff.sh: Replace proxy `unset`-based test with an
  actual measurement of parameter expansion.

* src/roff/nroff/tests/verbose_option_works.sh: Fix spurious failure of
  test for grep(1) supporting `-q` and `-x` options.  Continues commit
  e05cf6d3b3, 4 December.

Every time I have to deal with Solaris, it's like gazing into the Abyss.

A test failure of "src/roff/nroff/tests/verbose_option_works.sh" is
expected on Solaris 10, because 2 of the checks in that script verify
that option clustering works, and with Solaris 10 /bin/sh, it doesn't.
The test failure is therefore not spurious.  One can edit the installed
nroff script in place to use "/usr/xpg4/bin" in its shebang line.

src/roff/nroff/nroff.sh
@@ -23,7 +23,13 @@
 
 # Screen for shells non-conforming with POSIX Issue 4 (1994).
 badshell=yes
-unset groff_ku7kiodu || badshell=
+# Solaris 10 /bin/sh is so wretched that it not only doesn't support
+# standard parameter expansion, but it also writes diagnostic messages
+# to the standard output instead of standard error.
+if [ -n "$SHELL" ]
+then
+  "$SHELL" -c 'prog=${0##*/}' >/dev/null 2>&1 && badshell=
+fi
 
 if [ -n "$badshell" ]
 then

HACKING
@@ -246,11 +246,17 @@ Here are some portability notes on writing automated tests.
     };
     }
 
-* Solaris 10 cksum's output is non-conforming with XPG4.  It uses tabs
-  as field delimiters instead of spaces.
+* Solaris 10's /usr/bin/cksum output is non-conforming with XPG4.  It
+  uses tabs as field delimiters instead of spaces.
 
-* Solaris 10 tr exits with an error if you try to use a POSIX character
-  class (such as "[:cntrl:]") in any locale but "C".
+* Solaris 10's /usr/bin/grep is non-conforming with XPG4; it lacks
+  support for the `-E`, `-F`, `-q`, and `-x` options.
+
+* Solaris 10's /bin/sh is non-conforming with XPG4; it does not support
+  POSIX parameter expansion syntax.
+
+* Solaris 10's /usr/bin/tr exits with an error if you try to use a POSIX
+  character class (such as "[:cntrl:]") in any locale but "C".
 
 * Solaris 10's /usr/xpg4/bin/sh is non-conforming with XPG4.
   (Good job, guys!)

src/roff/nroff/tests/verbose_option_works.sh
@@ -18,7 +18,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-if ! echo foobar | grep -qx . >/dev/null 2>&1
+if ! echo foobar | grep -qx foobar >/dev/null 2>&1
 then
     echo "$0: grep command does not support -qx options; skipping" >&2
     exit 77 # skip

commit 0f2e7b537f4fbfc1f1a209bdc441f570ca7bdee1

Fix Savannah #68212.
* contrib/eqn2graph/eqn2graph.sh:
* contrib/grap2graph/grap2graph.sh:
* contrib/pic2graph/pic2graph.sh:
* src/preproc/eqn/neqn.sh:
* src/roff/nroff/nroff.sh: Use a subshell instead of the user's login
  shell `$SHELL` to evaluate features of the running shell.  The login
  shell need not be a POSIX shell script interpreter; it could be
  tcsh(1), rc(1), fish(1), or something exotic.

* src/roff/nroff/nroff.sh: Update fatal error diagnostic accordingly.

Fixes <https://savannah.gnu.org/bugs/?68212>.  [Problem
introduced by me in commit c34c6cc58c, 2024-12-07. --GBR]

contrib/eqn2graph/eqn2graph.sh
@@ -35,10 +35,7 @@ badshell=yes
 # Solaris 10 /bin/sh is so wretched that it not only doesn't support
 # standard parameter expansion, but it also writes diagnostic messages
 # to the standard output instead of standard error.
-if [ -n "$SHELL" ]
-then
-  "$SHELL" -c 'prog=${0##*/}' >/dev/null 2>&1 && badshell=
-fi
+(prog=${0##*/}) >/dev/null 2>&1 && badshell=
 
 if [ -n "$badshell" ]
 then

contrib/grap2graph/grap2graph.sh
@@ -34,10 +34,7 @@ badshell=yes
 # Solaris 10 /bin/sh is so wretched that it not only doesn't support
 # standard parameter expansion, but it also writes diagnostic messages
 # to the standard output instead of standard error.
-if [ -n "$SHELL" ]
-then
-  "$SHELL" -c 'prog=${0##*/}' >/dev/null 2>&1 && badshell=
-fi
+(prog=${0##*/}) >/dev/null 2>&1 && badshell=
 
 if [ -n "$badshell" ]
 then

contrib/pic2graph/pic2graph.sh
@@ -41,10 +41,7 @@ badshell=yes
 # Solaris 10 /bin/sh is so wretched that it not only doesn't support
 # standard parameter expansion, but it also writes diagnostic messages
 # to the standard output instead of standard error.
-if [ -n "$SHELL" ]
-then
-  "$SHELL" -c 'prog=${0##*/}' >/dev/null 2>&1 && badshell=
-fi
+(prog=${0##*/}) >/dev/null 2>&1 && badshell=
 
 if [ -n "$badshell" ]
 then

src/preproc/eqn/neqn.sh
@@ -24,10 +24,7 @@ badshell=yes
 # Solaris 10 /bin/sh is so wretched that it not only doesn't support
 # standard parameter expansion, but it also writes diagnostic messages
 # to the standard output instead of standard error.
-if [ -n "$SHELL" ]
-then
-  "$SHELL" -c 'prog=${0##*/}' >/dev/null 2>&1 && badshell=
-fi
+(prog=${0##*/}) >/dev/null 2>&1 && badshell=
 
 if [ -n "$badshell" ]
 then

src/roff/nroff/nroff.sh
@@ -27,10 +27,7 @@ badshell=yes
 # Solaris 10 /bin/sh is so wretched that it not only doesn't support
 # standard parameter expansion, but it also writes diagnostic messages
 # to the standard output instead of standard error.
-if [ -n "$SHELL" ]
-then
-  "$SHELL" -c 'prog=${0##*/}' >/dev/null 2>&1 && badshell=
-fi
+(prog=${0##*/}) >/dev/null 2>&1 && badshell=
 
 if [ -n "$badshell" ]
 then
@@ -73,10 +70,8 @@ do
       -[abCEikpRStUvzZ]*)
         if test -n "$badshell"
         then
-          # POSIX doesn't actually require $SHELL, but fortunately at
-          # least one craptastic non-conforming shell offers it.
           echo "$prog: option cluster '$thisarg' not supported with" \
-            "POSIX-non-conforming shell '$SHELL'" >&2
+            "POSIX-non-conforming shell" >&2
           exit 2
         fi
         remainder=${thisarg#-?}