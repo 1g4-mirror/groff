1274099f732216055af36604082da6ca901dd1cd
G. Branden Robinson <g.branden.robinson@gmail.com>
2026-05-24 22:26:08 -0500

    [groff]: Regression-test Savannah #68395.
    * src/roff/groff/tests/do-not-crash-on-missing-fschar-argument.sh: Do
      it.
    
    * src/roff/groff/groff.am (groff_TESTS): Run test.
    
    Test fails at this commit.

---
+++ src/roff/groff/tests/do-not-crash-on-missing-fschar-argument.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Copyright 2026 G. Branden Robinson
+#
+# This file is part of groff, the GNU roff typesetting system.
+#
+# groff is free software; you can redistribute it and/or modify it over
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# groff is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+groff="${abs_top_builddir:-.}/test-groff"
+
+# troff should not throw an assertion failure or call
+# `define_character()` with nothing to read from the input stream when
+# handling the `fschar` request.  Savannah #68395.
+
+echo '.fschar S' | "$groff" -T ps
+
+# vim:set autoindent expandtab shiftwidth=4 tabstop=4 textwidth=72:
