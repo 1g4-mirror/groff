1g4-mirror/groff

commit 20feac542d6686159121f08778f7c01ca8f19e86

[troff]: Fix blunders in `assert()` usage.
* src/roff/troff/input.cpp (assign_control_character)
  (assign_no_break_control_character): Do it.  I'm trying to make groff
  safe for builds using `NDEBUG`.  That can't be achieved if we wrap
  substantive function calls (or expressions with side effects) with
  `assert()`.

Continues commit e09bc6c083, 7 May 2023 (but not merged onto the master
branch until after the groff 1.23.0 release).

src/roff/troff/input.cpp
@@ -273,6 +273,7 @@ void assign_control_character()
       already_message = already_nbcc;
       do_nothing = true;
   }
+  bool assignment_worked = false;
   if (do_nothing)
     error("ignoring control character change request; %1%2 %3",
 	  is_invalid ? "cannot select invalid control character, and"
@@ -280,10 +281,11 @@ void assign_control_character()
   else if (is_invalid) {
     error("cannot select %1 as control character; using '.'",
 	  tok.description());
-    assert(curenv->set_control_character('.'));
+    assignment_worked = curenv->set_control_character('.');
   }
   else
-    assert(curenv->set_control_character(cc));
+    assignment_worked = curenv->set_control_character(cc);
+  assert(assignment_worked);
   skip_line();
 }
 
@@ -311,6 +313,7 @@ void assign_no_break_control_character()
       already_message = already_cc;
       do_nothing = true;
   }
+  bool assignment_worked = false;
   if (do_nothing)
     error("ignoring no-break control character change request; %1%2 %3",
 	  is_invalid ? "cannot select invalid no-break control"
@@ -319,10 +322,11 @@ void assign_no_break_control_character()
   else if (is_invalid) {
     error("cannot select %1 as no-break control character;"
 	  " using \"\'\"", tok.description());
-    assert(curenv->set_no_break_control_character('\''));
+    assignment_worked = curenv->set_no_break_control_character('\'');
   }
   else
-    assert(curenv->set_no_break_control_character(nbcc));
+    assignment_worked = curenv->set_no_break_control_character(nbcc);
+  assert(assignment_worked);
   skip_line();
 }
 

commit bbb7f0bf4ec792f34d7afe4612cca4d6ca858380

[troff]: Fix Savannah #68420.
* src/roff/troff/input.cpp (assign_control_character_request)
  (assign_no_break_control_character_request): Fix (further) blunders in
  assert(3) usage.  Spurious assertion failures occurred when user
  attempted to set a control character to one that is already in use as
  the other control character, or the escape character.

Fixes <https://savannah.gnu.org/bugs/?68420>.  Problem introduced by me
in commit 20feac542d, 2025-11-23.  Thanks to Martin Simmons for the
report.

ANNOUNCE: Acknowledge Martin.

ANNOUNCE
@@ -220,6 +220,7 @@ Hilmar Preuße
 Johannes Altmanninger
 John Gardner
 Karl Berry
+Martin Simmons
 Morten Bo Johansen
 Richard Purdie
 Ross Burton

src/roff/troff/input.cpp
@@ -321,7 +321,7 @@ static void assign_control_character_request() // .cc
   }
   else
     assignment_worked = curenv->set_control_character(cc);
-  assert(assignment_worked);
+  assert(do_nothing || assignment_worked);
   skip_line();
 }
 
@@ -364,7 +364,7 @@ static void assign_no_break_control_character_request() // .c2
   }
   else
     assignment_worked = curenv->set_no_break_control_character(nbcc);
-  assert(assignment_worked);
+  assert(do_nothing || assignment_worked);
   skip_line();
 }
 