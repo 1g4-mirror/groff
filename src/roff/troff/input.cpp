1g4-mirror/groff

commit 3f0c81a0a7ea7ba59ccbe6382ba1b42861b54a0e

[troff]: Refactor `cflags` request handler.
* src/roff/troff/input.cpp (set_character_flags_request): Refactor; use
  `token::is_any_character()` instead of `has_arg()` as loop control, in
  preparation for firming-up of the semantics and logic of request
  arguments to constitute space-separated lexical tokens, as our
  documentation defines "argument".

src/roff/troff/input.cpp
@@ -8858,7 +8858,7 @@ static void set_character_flags_request() // .cflags
       skip_line();
       return;
     }
-    while (has_arg()) {
+    while (tok.is_any_character()) {
       charinfo *ci = tok.get_charinfo(true /* is_mandatory */);
       if (0 /* nullptr */ == ci)
 	assert(0 == "attempted to use token without charinfo in"

commit 868dabcdb3c8944a6a6fb2cfcc7b82d1e90e7073

[troff]: Fix Savannah #68510.
* src/roff/troff/input.cpp (set_character_flags_request): Change
  condition of `while` loop from `tok.is_any_character()` to new
  side-effectful `read_any_character()` function.

Fixes <https://savannah.gnu.org/bugs/?68510>.  Problem introduced by me
in commit 3f0c81a0a7, 24 May.  Thanks to Dave Kemper for the report.

src/roff/troff/input.cpp
@@ -8893,7 +8893,7 @@ static void set_character_flags_request() // .cflags
     skip_line();
     return;
   }
-  while (tok.is_any_character()) {
+  while (read_any_character()) {
     charinfo *ci = tok.get_charinfo(true /* is_mandatory */);
     if (0 /* nullptr */ == ci)
       assert(0 == "attempted to use token without charinfo in"