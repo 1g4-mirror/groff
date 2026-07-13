1g4-mirror/groff

commit b039f8c3d1d2c00e3bb408768af2f8025b4c317c

[troff]: Drop warning diagnostic.
* src/roff/troff/input.cpp (token::next): Drop warning diagnostic.  DWB
  troff accepts a negative argument to the `\N` escape sequence; so
  should we.  (Interestingly, it seems to be difficult or impossible to
  get Heirloom Doctools to emit a "trout" `N` command--some table maps
  negative character indices to special character names.  For instance,
  we expect `N-1` but get `Chy`.  If no mapping exists for `\N`'s
  argument, it emits _no_ trout command for the character.)

src/roff/troff/input.cpp
@@ -2425,11 +2425,6 @@ void token::next()
       case 'N':
 	if (!read_delimited_number(&val, 0))
 	  break;
-	if (val < 0) {
-	  warning(WARN_CHAR, "character with negative index %1 is"
-		  " invalid; ignoring", val);
-	  break;
-	}
 	type = TOKEN_INDEXED_CHAR;
 	return;
       case 'o':

commit feb92d80933f13bd639d3d4962c279463e729188

[troff]: Fix Savannah #68521.
* src/roff/troff/input.cpp (token::next): Perform range check on
  parameter to `\N` escape sequence; throw error diagnostic if it fails.

Fixes <https://savannah.gnu.org/bugs/?68521>.  Problem introduced by me
in commit b039f8c3d1, 2025-03-29, an attempt to improve compatibility
with DWB troff.  (But as noted in that commit, DWB troff appears not to
_exercise_ its freedom to employ negative character indices.)

src/roff/troff/input.cpp
@@ -2687,6 +2687,11 @@ void token::next()
 	// The argument is a glyph index, which is dimensionless.
 	if (!read_delimited_measurement(&val, 0 /* dimensionless */))
 	  break;
+	if (val < 0) {
+	  error("expected nonnegative indexed character value; got %1",
+		val);
+	  break;
+	}
 	type = TOKEN_INDEXED_CHAR;
 	return;
       case 'o':