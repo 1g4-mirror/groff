1g4-mirror/groff

commit 85c8daa27d73da849e221b1ca250d414f6902bd7

src/roff/troff/env.cpp: Slightly refactor.
* src/roff/troff/env.cpp: Make request handler functions more consistent
  such that calling `skip_line()` is the last thing they do before
  returning.  The goal is to build confidence that request handlers
  reliably leave the input stream in a well-defined state.  Doing so
  would prevent several bugs I introduced (and resolved, with help from
  fellow developers) in the groff 1.23.0 development cycle, but also
  cases like Savannah #68064, which lurked insidiously for about 35
  years.

  (read_hyphenation_patterns_from_file): Stop calling `tok.next()`
  before returning.

  (load_hyphenation_patterns_from_file_request)
  (append_hyphenation_patterns_from_file_request): Call `skip_line()`
  before returning.

src/roff/troff/env.cpp
@@ -4663,7 +4663,6 @@ static void read_hyphenation_patterns_from_file(bool append)
       current_language->patterns.read_patterns_file(filename, append,
 	&current_language->exceptions);
   }
-  tok.next();
 }
 
 static void load_hyphenation_patterns_from_file_request() // .hpf
@@ -4675,8 +4674,7 @@ static void load_hyphenation_patterns_from_file_request() // .hpf
     return;
   }
   read_hyphenation_patterns_from_file(false /* append */);
-  // No `skip_line()` here; the above function calls
-  // `read_rest_of_line_as_argument()` and `tok.next()`.
+  skip_line();
 }
 
 static void append_hyphenation_patterns_from_file_request() // .hpfa
@@ -4688,8 +4686,7 @@ static void append_hyphenation_patterns_from_file_request() // .hpfa
     return;
   }
   read_hyphenation_patterns_from_file(true /* append */);
-  // No `skip_line()` here; the above function calls
-  // `read_rest_of_line_as_argument()` and `tok.next()`.
+  skip_line();
 }
 
 // Most hyphenation functionality is environment-specific; see

commit 8e458a0ab15bd3b7a20e69b2f7130769be6f983a

[troff]: Fix Savannah #68524.
* src/roff/troff/env.cpp (load_hyphenation_patterns_from_file_request)
  (append_hyphenation_patterns_from_file_request): Before returning,
  call `tok.next()` instead of `skip_line()`.  Annotate why: doing so
  prevents the formatter from ignoring the input line after the request.

Fixes <https://savannah.gnu.org/bugs/?68524>.  Problem introduced by me
in commit 85c8daa27d, 1 April.  Thanks to Dave Kemper for the report.

src/roff/troff/env.cpp
@@ -4889,7 +4889,9 @@ static void load_hyphenation_patterns_from_file_request() // .hpf
     return;
   }
   update_hyphenation_patterns_from_file(false /* appending */);
-  skip_line();
+  // No skip_line() here; update_hyphenation_patterns_from_file() calls
+  // read_rest_of_line_as_argument().
+  tok.next();
 }
 
 static void append_hyphenation_patterns_from_file_request() // .hpfa
@@ -4901,7 +4903,9 @@ static void append_hyphenation_patterns_from_file_request() // .hpfa
     return;
   }
   update_hyphenation_patterns_from_file(true /* appending */);
-  skip_line();
+  // No skip_line() here; update_hyphenation_patterns_from_file() calls
+  // read_rest_of_line_as_argument().
+  tok.next();
 }
 
 // Most hyphenation functionality is environment-specific; see