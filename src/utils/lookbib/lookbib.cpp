741a7eea5f78b5eaf3e6e4f615f6280db9021d2d
G. Branden Robinson <g.branden.robinson@gmail.com>
2026-09-12 03:26:19 -0500

    [lookbib]: Update/resync usage msg & man synopsis.
    * src/utils/lookbib/lookbib.1.man (Synopsis):
    * src/utils/lookbib/lookbib.cpp (usage): Rename inscrutable `-t`
      parameter from "n" to "maximum-key-length".  Rename inscrutable `-i`
      parameter from (variously) "XYZ" or "string" to "fields".

--- src/utils/lookbib/lookbib.cpp
+++ src/utils/lookbib/lookbib.cpp
@@ -48,7 +48,7 @@ extern "C" const char *Version_string;
 static void usage(FILE *stream)
 {
   fprintf(stream,
-	  "usage: %s [-i XYZ] [-t N] database ...\n"
+	  "usage: %s [-i fields] [-t maximum-key-length] database ...\n"
 	  "usage: %s {-v | --version}\n"
 	  "usage: %s --help\n",
 	  program_name, program_name, program_name);
